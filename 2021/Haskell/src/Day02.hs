45b4b88ca2e642dbcfe56ef316d588dd7d0292bc
Sondre Skaflem Lunde <sondre.skaflem.lunde@dnb.no>
2021-12-02 09:07:51 +0100

    Improve day 2

--- 2021/Haskell/src/Day02.hs
+++ 2021/Haskell/src/Day02.hs
@@ -1,22 +1,16 @@
+{-# LANGUAGE ViewPatterns #-}
 module Day02 where
 
 import Lib (tuple)
 import Data.List (foldl')
-import Linear (V2(..), V3(..), R2, _xy)
-import Control.Lens (productOf, each, (&))
+import Linear (V2(..), V3(..), R2, _xy, _xz)
+import Control.Lens (productOf, each, (&), Lens')
 
-run :: (R2 t, Applicative t, Num (t Int)) => [t Int -> t Int] -> Int
-run = productOf (_xy . each) . foldl' (&) (pure 0)
+run :: (R2 t, Applicative t, Num (t Int)) => Lens' (t Int) (V2 Int) -> [t Int -> t Int] -> Int
+run f = productOf (f . each) . foldl' (&) (pure 0)
 
-part1 :: (String, Int) -> V2 Int -> V2 Int
-part1 (str, v) = 
-  case str of
-    "up" -> (+ V2 0 (-v))
-    "down" -> (+ V2 0 v)
-    "forward" -> (+ V2 v 0)
-
-part2 :: (String, Int) -> V3 Int -> V3 Int
-part2 (str, v) = 
+encode :: (String, String) -> V3 Int -> V3 Int
+encode (str, read -> v) = 
   case str of 
     "up" -> (+ V3 0 0 (-v))
     "down" -> (+ V3 0 0 v)
@@ -24,10 +18,10 @@ part2 (str, v) =
 
 main :: IO ()
 main = do
-  input <- map (fmap read . tuple . words) . lines <$> readFile "../data/day02.in"
+  input <- map (encode . tuple . words) . lines <$> readFile "../data/day02.in"
 
-  print . run $ map part1 input
-  print . run $ map part2 input
+  print . run _xz $ input
+  print . run _xy $ input
 
 -- 2039256
 -- 1856459736
