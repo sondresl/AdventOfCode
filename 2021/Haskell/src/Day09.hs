6e44835a271ee8d1147353d5066ec7e472b20e04
sondresl <sondreslunde@gmail.com>
2021-12-09 06:49:04 +0100

    Day 9

--- 2021/Haskell/src/Day09.hs
+++ 2021/Haskell/src/Day09.hs
@@ -1,36 +1,26 @@
 module Day09 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
+import Lib (bfs, parseAsciiMap, neighbours4)
+import Linear (V2(..))
+import Data.Maybe (mapMaybe)
+import Data.List.Extra (sortBy)
+import Control.Applicative (liftA2)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
 
-part1 input = undefined
-
-part2 input = undefined
+basin :: Map (V2 Int) Int -> (V2 Int, Int) -> Int
+basin input (pos, val) = length $ bfs [pos] f
+  where
+    f = filter (liftA2 (&&) (val <) (/= 9) . flip (Map.findWithDefault 0) input) . neighbours4
 
 main :: IO ()
 main = do
+  input <- parseAsciiMap (Just . read . pure) <$> readFile "../data/day09.in"
+  let lowPoint k v = all (v <) . mapMaybe (`Map.lookup` input) $ neighbours4 k
+      lowPoints = Map.toList $ Map.filterWithKey lowPoint input
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
+  print . sum . map ((+1) . snd) $ lowPoints
+  print . product . take 3 . sortBy (flip compare) $ map (basin input) lowPoints
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day09.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 545
+-- 950600
