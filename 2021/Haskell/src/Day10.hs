ff769285760cb3154dd07b4cacb00752b21bf31c
sondresl <sondreslunde@gmail.com>
2021-12-10 07:26:28 +0100

    Day 10

--- 2021/Haskell/src/Day10.hs
+++ 2021/Haskell/src/Day10.hs
@@ -1,36 +1,41 @@
+{-# LANGUAGE LambdaCase #-}
 module Day10 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
+import Data.List.Extra (sort)
 
-part1 input = undefined
+paren :: String -> (Int, String)
+paren = go []
+  where
+    go [] [] = (0, [])
+    go [] (r:rs) = go [r] rs
+    go stack [] = (0, stack)
+    go stack@(s:ss) (x:xs) 
+     | x `elem` "([{<" = go (x : stack) xs
+     | (s,x) `elem` [('(', ')'), ('{', '}'), ('[', ']'), ('<', '>')] = go ss xs
+     | otherwise = (score x, [])
 
-part2 input = undefined
+score = \case
+    ')' -> 3
+    ']' -> 57
+    '}' -> 1197
+    '>' -> 25137
+
+score' = \case 
+  '(' -> 1
+  '[' -> 2
+  '{' -> 3
+  '<' -> 4
 
 main :: IO ()
 main = do
+  input <- lines <$> readFile "../data/day10.in"
+  let res = map paren input
+  print . sum $ map fst res
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
+  let remaining = foldl (\acc new -> acc * 5 + score' new) 0
+  print . (\xs -> sort xs !! (length xs `div` 2)) 
+        . map (remaining . snd) 
+        $ filter ((== 0) . fst) res
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day10.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 411471
+-- 3122628974
