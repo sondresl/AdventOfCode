239976f4a5da913ce1f9ddb6a89311804cd6edf6
sondresl <sondreslunde@gmail.com>
2021-12-21 21:03:25 +0100

    Add notes to day 21

--- 2021/Haskell/app/Main.hs
+++ 2021/Haskell/app/Main.hs
@@ -50,6 +50,6 @@ main = do
   -- run "Day16:" Day16.main
   -- run "Day17:" Day17.main
   -- run "Day18:" Day18.main
-  -- run "Day19:" Day19.main
+  run "Day19:" Day19.main
   -- run "Day20:" Day20.main
-  run "Day21:" Day21.main
+  -- run "Day21:" Day21.main
--- 2021/Haskell/src/Day21.hs
+++ 2021/Haskell/src/Day21.hs
@@ -1,8 +1,8 @@
 module Day21 where
 
 import Lib (tuple, freqs, memo4)
-import Advent.Coord (invert)
 import Linear (_x, V2(..))
+import Advent.Coord (invert)
 import Control.Lens (view)
 import Control.Applicative (liftA3)
 import Data.Map (Map)
@@ -16,20 +16,20 @@ play (score1, pos1) (score2, pos2) cnt rolls
     pos' = (pos1 + sum (take 3 rolls)) `mod` 10
     score' = score1 + pos' + 1
 
-solve' :: Int -> Int -> Int -> Int -> V2 Int
-solve' = memo4 $ \p1 s1 p2 s2 -> sum $ do
+solve :: Int -> Int -> Int -> Int -> V2 Int
+solve = memo4 $ \p1 s1 p2 s2 -> sum $ do
   (v, freq) <- Map.toList (freqs (liftA3 (\a b c -> a + b + c) [1,2,3] [1,2,3] [1,2,3]))
   let p1' = (p1 + v) `mod` 10
       s1' = s1 + p1' + 1
   pure $ if s1' >= 21 
            then V2 freq 0
-           else (* V2 freq freq) . invert $ solve' p2 s2 p1' s1'
+           else (* V2 freq freq) . invert $ solve p2 s2 p1' s1'
 
 main :: IO ()
 main = do
   (one, two) <- parseInput <$> readFile "../data/day21.in"
   print $ play (0, one - 1) (0, two - 1) 0 (cycle [1..100])
-  print $ view _x $ memo4 solve' (one - 1) 0 (two - 1) 0
+  print $ view _x $ memo4 solve (one - 1) 0 (two - 1) 0
 
 parseInput :: String -> (Int, Int)
 parseInput = tuple . map (read . pure . last) . lines
