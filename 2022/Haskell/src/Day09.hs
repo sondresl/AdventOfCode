6fbd5a32f8241a967ef03e07deaf84ad21ffa3a0
Sondre Skaflem Lunde <sondre.skaflem.lunde@dnb.no>
2022-12-09 08:38:21 +0100

    Day 9

--- 2022/Haskell/src/Day09.hs
+++ 2022/Haskell/src/Day09.hs
@@ -1,36 +1,39 @@
 module Day09 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
+import Lib (mannDist, ordNub)
+import Linear hiding (trace)
+import Advent.Coord (origin, right, left, up, down, Coord)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-part1 input = undefined
-
-part2 input = undefined
+tailPos :: [Coord] -> [Coord]
+tailPos xs = origin : go (Set.singleton origin) origin xs
+  where
+    truncPoint (V2 x y) = V2 (trunc x) (trunc y)
+    trunc x = if x > 0 then 1 else if x < 0 then (-1) else 0
+    go been p@(V2 px py) [] = []
+    go been p@(V2 px py) (x@(V2 xx xy):xs)
+      | px /= xx && py /= xy && mannDist x p > 2 || 
+       (px == xx || py == xy) && mannDist x p == 2 = 
+        let new = p + truncPoint (x - p)
+         in new : go (Set.insert new been) new xs
+      | otherwise = go been p xs
 
 main :: IO ()
 main = do
+  input <- parseInput <$> readFile "../data/day09.in"
+  let headPos = scanl (+) origin input
+  print $ length $ ordNub $ tailPos headPos
+  print . length . ordNub . (!! 9) $ iterate tailPos headPos
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
+parseInput :: String -> [Coord]
+parseInput = concatMap (f . words) . lines
+  where
+    f ["R", read -> n] = replicate n right
+    f ["L", read -> n] = replicate n left 
+    f ["U", read -> n] = replicate n up 
+    f ["D", read -> n] = replicate n down 
+    f e = error (show e)
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day09.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 6197
+-- 2562
