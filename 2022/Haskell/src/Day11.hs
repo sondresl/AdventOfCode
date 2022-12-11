ad8779752a0e84501fe9809017eef0122539f939
sondresl <sondreslunde@gmail.com>
2022-12-11 19:59:21 +0100

    Simplify day 11

--- 2022/Haskell/src/Day11.hs
+++ 2022/Haskell/src/Day11.hs
@@ -1,33 +1,32 @@
 module Day11 where
 
 import Lib (freqs)
-import Data.List.Extra (splitOn, sort)
+import Data.Ord (Down(Down))
+import Data.List.Extra (splitOn, sortOn)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Text.ParserCombinators.Parsec (string, many1, digit, parse, newline, sepBy, oneOf, try, char, (<|>))
 
-data Monkey = Monkey
-  { number :: Int
-  , items :: [Int]
-  , modOp :: Int -> Int
-  , op :: Int -> Int
-  , next :: Int -> Int
-  }
+type Monkey =
+  ( Int -> Int -- Increase worry level
+  , Int -> Int -- Test on worry level, return monkey throwing to
+  )
 
-type Factors = Map Int Int
+runGift
+  :: (Int -> Int)   -- Calming function, keeping the worry level in check
+  -> Map Int Monkey -- Monkey id to functions
+  -> (Int, Int)     -- (current monkey ID, worry level)
+  -> (Int, Int)     -- New of ^^
+runGift calmF mp (mnk, worry) = ((,) =<< next) $ calmF (op worry)
+  where (op, next) = mp Map.! mnk
 
-runGift :: (Int -> Int) -> Map Int Monkey -> (Int, Factors) -> (Int, Factors)
-runGift f mp (mnk, worry) = (to, worry')
-  where
-    current = mp Map.! mnk
-    worry' = Map.mapWithKey (\k i -> let (Monkey _ _ modOp _ _) = mp Map.! k in modOp (f $ op current i)) worry
-    to = next current (worry' Map.! mnk)
-
--- Round number increases when the monkey ID goes down, since
--- then the number will only be evaluated the next time around.
-countRounds :: Int -> [(Int, Factors)] -> Map Int Int
+countRounds
+  :: Int          -- Total number of rounds we want to play
+  -> [(Int, Int)] -- Every played step of monkey and worry level
+  -> Map Int Int  -- Number of inspections by monkey
 countRounds rs = freqs . f 0 . (zip <*> tail)
-  where 
+  where
+    f round [] = error "countRounds should always terminate"
     f round (((a,b), (x,y)):xs)
       | round == rs = []
       | x < a = a : f (succ round) xs
@@ -35,31 +34,30 @@ countRounds rs = freqs . f 0 . (zip <*> tail)
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "../data/day11.in"
-  let gifts = concatMap (\(Monkey num xs op modOp test) -> map (num,) xs) $ Map.elems input
-  let starts (num, val) = (num,) $ Map.map (\(Monkey _ _ modOp _ _) -> modOp val) input
-      part2 = Map.unionsWith (+) $ map (countRounds 10000 . iterate (runGift id        input) . starts) gifts
-      score = product . take 2 . reverse . sort . Map.elems
-  print $ score part2
+  (Map.fromList -> input, concat -> gifts, product -> mods) <- unzip3 . parseInput <$> readFile "../data/day11.in"
+  let run n f = Map.unionsWith (+) $ map (countRounds n . iterate (runGift f input)) gifts
+      score = product . take 2 . sortOn Down . Map.elems
+  print . score $ run    20 (`div`    3)
+  print . score $ run 10000 (`mod` mods)
 
-parseInput :: String -> Map Int Monkey
-parseInput = Map.fromList . map (\x -> (number x, x)) . either (error . show) id . traverse (parse p "") . splitOn "\n\n"
+parseInput :: String -> [((Int, Monkey), [(Int, Int)], Int)]
+parseInput = either (error . show) id . traverse (parse p "") . splitOn "\n\n"
   where
     p = do
-      num <- read <$> (string "Monkey " *> many1 digit <* string ":" <* newline)
+      num   <- read <$> (string "Monkey " *> many1 digit <* string ":" <* newline)
       items <- string "  Starting items: " *> sepBy (many1 digit) (string ", ") <* newline
-      op <- string "  Operation: new = old " *> oneOf "*+"
-      v <- try (char ' ' *> many1 digit <* newline) <|> (char ' ' *> string "old" <* newline)
-      test <- read <$> (string "  Test: divisible by " *> many1 digit <* newline)
-      true <- read <$> (string "    If true: throw to monkey " *> many1 digit <* newline)
+      op    <- string "  Operation: new = old " *> oneOf "*+"
+      value <- try (char ' ' *> many1 digit <* newline) <|> (char ' ' *> string "old" <* newline)
+      test  <- read <$> (string "  Test: divisible by " *> many1 digit <* newline)
+      true  <- read <$> (string "    If true: throw to monkey " *> many1 digit <* newline)
       false <- read <$> (string "    If false: throw to monkey " *> many1 digit)
-      let modOp x = x `mod` test
-          opF = case (v, op) of 
-                  ("old", _) -> (^2)
-                  (n, '+') -> (read v +)
-                  (n, '*') -> (read v *)
-          nextF y = if y == 0 then true else false
-      pure $ Monkey num (map read items) modOp opF nextF
+      let opF = case (value, op) of
+                  ("old", '*') -> (^2)
+                  (    n, '*') -> (read value *)
+                  (    n, '+') -> (read value +)
+                  _ -> error "opF"
+          nextF y = if y `mod` test == 0 then true else false
+      pure ((num, (opF, nextF)), map ((num,) . read) items, test)
 
 -- 69918
 -- 19573408701
