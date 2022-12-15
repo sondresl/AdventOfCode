e26bed46bd2856e48005f83765198ed0c5f51f99
sondresl <sondreslunde@gmail.com>
2022-12-15 19:46:55 +0100

    Cleanup day 14

--- 2022/Haskell/src/Day14.hs
+++ 2022/Haskell/src/Day14.hs
@@ -8,6 +8,8 @@ import Control.Lens (view)
 import Data.List.Extra (splitOn, find)
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Control.Monad (foldM)
+import Control.Monad.Identity
 
 type Rock = Set (V2 Int)
 
@@ -17,33 +19,20 @@ allRock xs = Set.fromList $ concatMap (concatMap f . (zip <*> tail)) xs
     f (from, to) = takeUntil (== to) $ iterate (+ dir) from
       where dir = signum $ to - from
 
-run :: (Rock -> V2 Int -> Maybe (V2 Int)) -> Rock -> Rock
-run fall rock = go (V2 500 0)
-  where
-    go pos = maybe rock (`Set.insert` rock) (fall rock pos)
-
-part1 :: Int -> Rock -> V2 Int -> Maybe (V2 Int)
-part1 lowestPoint rock p
-  | view _y p == lowestPoint = Nothing
-  | Just p <- find (`Set.notMember` rock) [up + p, left + up + p, right + up + p] 
-    = part1 lowestPoint rock p
-  | otherwise = Just p
-
-part2 :: Int -> Rock -> V2 Int -> Maybe (V2 Int)
-part2 lowestPoint rock p
-  | view _y p == lowestPoint = Just p
-  | Just p <- find (`Set.notMember` rock) [up + p, left + up + p, right + up + p] 
-    = part2 lowestPoint rock p
-  | otherwise = Just p
+run :: Monad m => (Rock -> m Rock) -> Int -> Rock -> V2 Int -> m Rock
+run f lowestPoint rock p@(V2 x y)
+  | y == lowestPoint = f rock
+  | p `Set.member` rock = pure rock
+  | otherwise = Set.insert p <$> foldM (run f lowestPoint) rock [up + p, left + up + p, right + up + p] 
 
 main :: IO ()
 main = do
   input <- allRock . parseInput <$> readFile "../data/day14.in"
-  let lowestPoint = Set.findMax $ Set.map (view _y) input
-      Just r1 = firstRepeatOn Set.size $ iterate (run (part1 lowestPoint)) input
-      Just r2 = firstRepeatOn Set.size $ iterate (run (part2 (lowestPoint + 1))) input
-  print $ Set.size r1 - Set.size input
-  print $ Set.size r2 - Set.size input
+  let lowestPoint = 2 + Set.findMax (Set.map (view _y) input)
+  let Left p1     = run Left     lowestPoint input (V2 500 0)
+  let Identity p2 = run Identity lowestPoint input (V2 500 0)
+  print $ Set.size p1 - Set.size input
+  print $ Set.size p2 - Set.size input
 
 parseInput :: String -> [[V2 Int]]
 parseInput = map (map (uncurry V2 . tuple. map read . splitOn ",") . splitOn " -> ") . lines
