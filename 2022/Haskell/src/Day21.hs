8320904ab5895cfd1d67e53fd8489c016b796045
sondresl <sondreslunde@gmail.com>
2022-12-21 23:09:25 +0100

    Cleanup day 21

--- 2022/Haskell/src/Day21.hs
+++ 2022/Haskell/src/Day21.hs
@@ -2,34 +2,28 @@ module Day21 where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.MemoTrie (memo)
+import Control.Lens ((<&>))
 
-type Monkeys = Map String (Either Int (String, Int -> Int -> Int, String))
+type Op = Int -> Int -> Int
+type Monkeys = Map String (Either Int (String, Op, String))
 
 compute :: Monkeys -> String -> Int
-compute mp = calc
-  where
-    calc = memo $ \x -> case mp Map.! x of
-               Left i -> i
-               Right (x, op, y) -> calc x `op` calc y
-
-compute2 :: Monkeys -> (Bool, Int, Int)
-compute2 mp = let (x, y) = (calc a, calc b) in (x == y, x, y)
-  where
-    Right (a,_,b) = mp Map.! "root"
-    calc = memo $ \x -> case mp Map.! x of
-               Left i -> i
-               Right (x, op, y) -> calc x `op` calc y
+compute mp = (res Map.!)
+  where res = mp <&> \case 
+                       Left i -> i
+                       Right (x, op, y) -> (res Map.! x) `op` (res Map.! y)
 
 part2 :: Monkeys -> Int
-part2 mp = go 0 100000000000  -- assume greater than 2
+part2 mp = go 0 100000000000
   where
-    go lower move = 
-      let (res, a, b) = compute2 $ Map.insert "humn" (Left lower) mp
+    root mp = let Right (a,_,b) = mp Map.! "root"
+               in (compute mp a, compute mp b)
+    go lower step = 
+      let (a, b) = root $ Map.insert "humn" (Left lower) mp
        in case a `compare` b of
             EQ -> lower
-            LT -> go (lower - move) (move `div` 10)
-            GT -> go (lower + move) move
+            LT -> go (lower - step) (step `div` 10)
+            GT -> go (lower + step) step
 
 main :: IO ()
 main = do
@@ -37,7 +31,7 @@ main = do
   print $ compute input "root"
   print $ part2 input
 
-parseInput :: String -> Map String (Either Int (String, Int -> Int -> Int, String))
+parseInput :: String -> Monkeys
 parseInput = Map.fromList . map (f . words) . lines
   where
     f [init -> name, num] = (name, Left (read num))
