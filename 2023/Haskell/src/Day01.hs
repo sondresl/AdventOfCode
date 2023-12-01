0ca9aaf0552c832383aa633262654cf01eaf22ce
Sondre Skaflem Lunde <sondre.skaflem.lunde@dnb.no>
2023-12-01 14:22:27 +0100

    Day 1 2023

--- 2023/Haskell/src/Day01.hs
+++ 2023/Haskell/src/Day01.hs
@@ -1,41 +1,27 @@
 module Day01 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.RawString.QQ
-import Text.ParserCombinators.Parsec hiding (count)
+import Lib (lookupJust)
+import Data.Maybe (mapMaybe)
+import Data.List.Extra (isPrefixOf, find, tails)
 
-part1 input = undefined
+ints, numbers :: [String]
+ints = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] <> ints
 
-part2 input = undefined
+convert :: [(String, Int)]
+convert = zip numbers (cycle [1..9])
+
+solve :: [String] -> String -> Int
+solve comps input = head opts * 10 + last opts
+  where opts = mapMaybe findNumber $ tails input
+        findNumber str = (`lookupJust` convert) <$> find (`isPrefixOf` str) comps
 
 main :: IO ()
 main = do
+  input <- lines <$> readFile "../data/day01.in"
+  let run f = sum $ map (solve f) input
+  print $ run ints
+  print $ run numbers
 
-  let run str input = do
-        putStrLn str
-        print input
-
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" testInput
-
-  -- input <- parseInput <$> readFile "../data/day01.in"
-  -- run "\nActual:\n\n" input
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
-
-testInput = [r|
-|]
+-- 55477
+-- 54431
