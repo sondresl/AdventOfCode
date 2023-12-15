1e183f211ac95d85b493cd64d815b014b78ab8a7
Sondre Skaflem Lunde <sondre.skaflem.lunde@dnb.no>
2023-12-15 12:24:09 +0100

    day 15

--- 2023/Haskell/src/Day15.hs
+++ 2023/Haskell/src/Day15.hs
@@ -1,41 +1,46 @@
 module Day15 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
+import Lib (tuple)
+import Data.List.Extra (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.RawString.QQ
-import Text.ParserCombinators.Parsec hiding (count)
+import Data.Char (ord)
 
-part1 input = undefined
+score :: Int -> Char -> Int
+score acc = (`rem` 256) .(*17) . (+ acc) . ord
 
-part2 input = undefined
+replaceTup xs (lab, val) =
+  case break ((== lab) . fst) xs of
+    (before, []) -> before <> [(lab, read val)]
+    (before, _:after) -> before <> [(lab, read val)] <> after
+
+removeTup :: [(String, Int)] -> String -> [(String, Int)]
+removeTup xs lab = filter ((/= lab) . fst) xs
+
+labels :: Map Int [(String, Int)] -> String -> Map Int [(String, Int)]
+labels mp x
+  | '=' `elem` x = let (lab, val) = tuple $ splitOn "=" x 
+                       sc = foldl score 0 lab
+                    in case Map.lookup sc mp of
+                         Nothing -> Map.insert sc [(lab, read val)] mp
+                         Just v -> Map.insert sc (replaceTup v (lab, val)) mp
+  | '-' `elem` x = let (lab, _) = tuple $ splitOn "-" x 
+                       sc = foldl score 0 lab
+                    in case Map.lookup sc mp of
+                         Nothing -> mp
+                         Just v -> Map.insert sc (removeTup v lab) mp
+
+scoreMap :: Map Int [(String, Int)] -> Int
+scoreMap mp = sum $ do
+  (box, xs) <- Map.assocs mp
+  (slot, lens) <- zip [1..] (map snd xs)
+  pure $ succ box * slot * lens
 
 main :: IO ()
 main = do
+  input <- splitOn "," . init <$> readFile "../data/day15.in"
+  print $ sum . map (foldl score 0) $ input
+  print $ scoreMap $ foldl labels Map.empty input
 
-  let run str input = do
-        putStrLn str
-        print input
-
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" testInput
-
-  -- input <- parseInput <$> readFile "../data/day15.in"
-  -- run "\nActual:\n\n" input
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
-
-testInput = [r|
-|]
+-- 507291
+-- 296921
