955fc80332c02c2376ac6418a188c91cd469ed04
sondresl <sondreslunde@gmail.com>
2023-12-21 08:01:39 +0100

    day 21 p1

--- 2023/Haskell/src/Day21.hs
+++ 2023/Haskell/src/Day21.hs
@@ -2,6 +2,7 @@ module Day21 where
 
 import Lib
 import Advent.Coord
+import Advent.Search
 import Data.Maybe
 import Control.Lens
 import Control.Monad
@@ -9,33 +10,56 @@ import Control.Monad.State
 import Data.List.Extra
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Text.RawString.QQ
 import Text.ParserCombinators.Parsec hiding (count)
+import Data.MemoTrie
 
-part1 input = undefined
+type Garden = Set Point
 
-part2 input = undefined
+part1 :: Garden -> Point -> Int
+part1 input start = length . filter (even . fst) . takeWhile ((< 65) . fst) $ walk input [(0, start)]
+
+part2 input start = undefined
+
+walk :: Garden -> [(Int, Point)] -> [(Int, Point)]
+walk input = memo $ \starts -> bfsOn snd starts nexts
+  where nexts (i, pos) = map (i+1,) . filter (`Set.member` input) $ ordinalNeighbours pos
 
 main :: IO ()
 main = do
 
   let run str input = do
+        let ps = Map.keysSet input
+            Just (start,_) = find ((== 'S') . snd) $ Map.assocs input
         putStrLn str
-        print input
-
-        -- print $ part1 input
+        print $ part1 ps start
         -- print $ part2 input
     
-  run "\nTest:\n\n" testInput
+  run "\nTest:\n\n" $ parseInput testInput
 
-  -- input <- parseInput <$> readFile "../data/day21.in"
-  -- run "\nActual:\n\n" input
+  input <- parseInput <$> readFile "../data/day21.in"
+  run "\nActual:\n\n" input
 
-parseInput = id
+parseInput = parseAsciiMap f
+  where f x = guard (x `elem` ".S") *> Just x
+
+-- 3809
 
 -- parseInput = either (error . show) id . traverse (parse p "") . lines
 --   where
 --     p = undefined
 
-testInput = [r|
+testInput = [r|...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
 |]
