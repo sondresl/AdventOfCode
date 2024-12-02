f19ecb115dbe90e5a34651a1e87a200e47f5c714
Sondre Skaflem Lunde <sondre.skaflem.lunde@dnb.no>
2024-12-02 09:06:09 +0100

    day 2 prettier

--- 2024/Haskell/src/Day02.hs
+++ 2024/Haskell/src/Day02.hs
@@ -2,26 +2,18 @@ module Day02 where
 
 import Lib (select, allNums, zipWithTail, count)
 
-part1 = count id . map (safe . map (uncurry subtract) . zipWithTail)
+solve :: ([Int] -> [[Int]]) -> [[Int]] -> Int
+solve f = count (any safe . diffs . f)
   where
+    diffs = map (map (uncurry subtract) . zipWithTail)
     safe xs = (all (> 0) xs || all (< 0) xs)
-            && all (<= 3) (map abs xs)
-
-part2 :: [[Int]] -> Int
-part2 input = safeReports + dampened
-  where
-    safeReports = count (safe . diffs) input
-    unsafe = filter (not . safe . diffs) input
-    dampened = count (not . null) $ map (filter (safe . diffs) . map snd . select) unsafe
-    diffs = map (uncurry subtract) . zipWithTail
-    safe xs = (all (> 0) xs || all (< 0) xs)
-            && all (<= 3) (map abs xs)
+            && all ((<= 3) . abs) xs
 
 main :: IO ()
 main = do
   input <- map allNums . lines <$> readFile "../data/day02.in"
-  print $ part1 input
-  print $ part2 input
+  print $ solve pure input
+  print $ solve (map snd . select) input
 
 -- 486
 -- 540
