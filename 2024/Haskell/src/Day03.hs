760eabca9079eb254e388b660080c1e3e910d99a
Sondre Skaflem Lunde <sondre.skaflem.lunde@dnb.no>
2024-12-03 16:57:58 +0100

    day 3 + readme stuff + bench

--- 2024/Haskell/src/Day03.hs
+++ 2024/Haskell/src/Day03.hs
@@ -1,35 +1,32 @@
 module Day03 where
 
-import Advent.Parsers (pNumber, pBetween)
+import Advent.Parsers (pBetween, pNumber, pFind)
 import Text.ParserCombinators.Parsec
-    (anyChar, char, string, option, (<|>), parse, try, Parser, sepBy, between)
+    (Parser, parse, sepBy, char, many, option, string, (<|>))
 import Data.Functor (($>))
 
-parseInput :: Parser [Int] -> String -> Int
-parseInput f = sum . either (error . show) id . parse f "" . concat . lines
+parseInput :: Parser a -> String -> a
+parseInput f = either (error . show) id . parse f ""
 
-mul :: Parser [Int] -> Parser [Int]
-mul f = do
-  [x, y] <- pBetween "mul(" ")" (pNumber `sepBy` char ',')
-  ((x * y):) <$> f
+mul :: Parser Int
+mul = pBetween "mul(" ")" ((*) <$> pNumber <* char ',' <*> pNumber)
 
--- Helpers
-enable, disable, disabled :: Parser [Int]
-enable   = string "do()"    *> part2
-disable  = string "don't()" *> disabled
-disabled = option [] $ try enable  <|> (anyChar *> disabled)
+part1 :: String -> Int
+part1 = parseInput (sum <$> many (pFind mul))
 
-part1 :: Parser [Int]
-part1 = option [] $ try (mul part1) <|> (anyChar *> part1)
-
-part2 :: Parser [Int]
-part2 = option [] $ try disable <|> try (mul part2) <|> (anyChar *> part2)
+part2 :: String -> Int
+part2 = parseInput enabled
+  where
+    enabled  = option 0 . pFind $ 
+      string "don't" *> disabled
+      <|> (+) <$> mul <*> enabled
+    disabled = option 0 . pFind $ string "do()" *> enabled
 
 main :: IO ()
 main = do
   input <- readFile "../data/day03.in"
-  print $ parseInput part1 input
-  print $ parseInput part2 input
+  print $ part1 input
+  print $ part2 input
 
 -- 175015740
 -- 112272912
