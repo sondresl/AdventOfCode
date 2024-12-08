025bad3554a784446bbae803ba445301a393952d
Sondre Lunde <sondre@Sondres-MacBook-Pro-2.local>
2024-12-08 12:36:21 +0100

    day 8 cleanup

--- 2024/Haskell/src/Day08.hs
+++ 2024/Haskell/src/Day08.hs
@@ -1,36 +1,31 @@
 module Day08 where
 
-import Lib (within, findBounds, parseAsciiMap, combinations)
+import Lib (invertMap, select, within, findBounds, parseAsciiMap)
 import Advent.Coord (Coord)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Linear
-
-data Location = Space | Antenna Char
-  deriving (Show, Eq)
-
-antinodes :: ([Coord] -> [Coord]) -> (Int, Int, Int, Int) -> [Coord] -> Set Coord
-antinodes f bounds cs = Set.fromList $ do
-  [a, b] <- combinations 2 cs
-  let diff = a - b
-      one = f . takeWhile (within bounds) $ iterate (+ diff) a
-      two = f . takeWhile (within bounds) $ iterate (subtract diff) b
-  one <> two
 
 main :: IO ()
 main = do
   (bounds, ants) <- parseInput <$> readFile "../data/day08.in"
-  print $ Set.size $ foldMap (antinodes (take 1 . drop 1) bounds) ants
-  print $ Set.size $ foldMap (antinodes id bounds) ants
+
+  let antinodes pick xs = Set.fromList $ do
+        (a, as) <- select xs
+        b <- as
+        let diff = a - b
+        pick . takeWhile (within bounds) $ iterate (+ diff) a
+
+  print $ Set.size $ foldMap (antinodes (take 1 . drop 1)) ants
+  print $ Set.size $ foldMap (antinodes id) ants
     
 parseInput :: String -> ((Int, Int, Int, Int), [[Coord]])
-parseInput input = (bounds, Map.elems ants')
+parseInput input = (bounds, Map.elems antennas')
   where
-    ants = parseAsciiMap Just input
-    bounds = findBounds [ pos | (pos, _) <- Map.assocs ants ]
-    ants' = Map.fromListWith (<>) [ (c, [pos]) | (pos, c) <- Map.assocs ants 
-                                               , c /= '.' ]
+    antennas = invertMap $ parseAsciiMap (Just . pure) input
+    antennas' = Map.delete '.' antennas
+    bounds = findBounds $ antennas Map.! '.'
+
 -- 371
 -- 1229
