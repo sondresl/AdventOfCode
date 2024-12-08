db29a5b441ceba5063d02407b1adeb871370ee1d
Sondre Lunde <sondre@Sondres-MacBook-Pro-2.local>
2024-12-08 08:35:12 +0100

    cleanup day 7

--- 2024/Haskell/src/Day07.hs
+++ 2024/Haskell/src/Day07.hs
@@ -3,11 +3,12 @@ module Day07 where
 import Lib (allNums)
 import Data.Maybe (mapMaybe, listToMaybe)
 import Control.Monad (guard)
+import Data.List (partition)
 
 type Op = Int -> Int -> Int
 
-combine :: [Op] -> [Int] -> Maybe Int
-combine ops (num:x:xs) = listToMaybe $ go x xs
+combine :: [Op] -> [Int] -> Bool
+combine ops (num:x:xs) = not . null $ go x xs
   where
     go total [] = guard (total == num) *> pure num
     go total (x:xs) = do
@@ -20,10 +21,11 @@ main :: IO ()
 main = do
   input <- map allNums . lines <$> readFile "../data/day07.in"
 
-  print $ sum $ mapMaybe (combine [(*),(+)]) input
+  let (p1, rest) = partition (combine [(*),(+)]) input 
+  print . sum . map head $ p1
 
-  let f x y = read (show x <> show y)
-  print $ sum $ mapMaybe (combine [(*),(+),f]) input
+  let f a b = a * 10 ^ (floor (logBase 10 (fromIntegral b)) + 1) + b
+  print $ sum . map head . (p1 <>) $ filter (combine [(*),(+),f]) rest
 
 -- 2437272016585
 -- 162987117690649
