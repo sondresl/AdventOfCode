8bf5ea93e65b253f89e9ccebe1d90f4cb1a8da99
Sondre Lunde <sondrelunde@Sondre-sin-MacBook-Pro.local>
2024-12-09 18:13:37 +0100

    improve day 9

--- 2024/Haskell/src/Day09.hs
+++ 2024/Haskell/src/Day09.hs
@@ -1,53 +1,54 @@
 module Day09 where
 
-import Data.Maybe
 import Data.Foldable (toList)
-import Data.List.Extra (foldl')
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Sequence (Seq((:<|), (:|>), Empty), (><))
+import Data.List.Extra (foldl', intersperse)
+import Data.Sequence (Seq((:<|), (:|>), Empty))
 import qualified Data.Sequence as Seq
 
 type Id = Int
 type Size = Int
 
-data Disk = E Size | F Id Int
+-- Empty space and Files
+data Disk = E Size | F Id Size
   deriving (Show, Eq)
 
-enoughSpace :: Int -> Disk -> Bool
-enoughSpace size (E s) = s >= size
-enoughSpace size _ = False
+enoughSpace :: Size -> Disk -> Bool
+enoughSpace size = \case
+  E s   -> s >= size
+  F _ _ -> False
 
 move :: Seq Disk -> Seq Disk
-move Empty = Empty
-move (f@(F i size) :<| rest)               = f :<| move rest
-move (f@(E size)   :<| (rest :|> (E _)))   = move (f :<| rest)
-move (f@(E size)   :<| (rest :|> (F i s)))
-  | size > s  = F i s    :<| move (E (size - s) :<| rest)
-  | size < s  = F i size :<| move (rest :|> F i (s - size))
-  | size == s = F i s    :<| move rest
+move = \case
+  Empty -> Empty
+  F i s  :<| rest               -> F i s :<| move rest
+  E size :<| (rest :|> (E _))   -> move (E size :<| rest)
+  E size :<| (rest :|> (F i s))
+    | size > s  -> F i s    :<| move (E (size - s) :<| rest)
+    | size < s  -> F i size :<| move (rest :|> F i (s - size))
+    | size == s -> F i s    :<| move rest
 
 moveWhole :: Seq Disk -> Seq Disk
-moveWhole Empty = Empty
-moveWhole (f@(F _ _) :<| rest) = f :<| (moveWhole rest)
-moveWhole (front :|> E i) = (moveWhole front) :|> E i
-moveWhole (front :|> F i size) = 
-  case Seq.findIndexL (enoughSpace size) front of
-    Nothing -> (moveWhole front) :|> F i size
-    Just ix -> let (E s) = Seq.index front ix
-                   front' = if s == size 
-                                then Seq.update ix (F i size) front
-                                else Seq.insertAt ix (F i size) 
-                                   $ Seq.update ix (E $ s - size) front
-                in (moveWhole front') :|> (E size)
+moveWhole = \case
+  Empty              -> Empty
+  F i s :<| rest     -> F i s :<| moveWhole rest
+  front :|> E i      -> moveWhole front :|> E i
+  front :|> F i size -> case Seq.findIndexL (enoughSpace size) front of
+    Nothing -> moveWhole front  :|> F i size
+    Just ix -> moveWhole front' :|> E size
+      where
+        (E s) = Seq.index front ix
+        front' = if s == size
+                   then Seq.update ix (F i size) front
+                   else Seq.insertAt ix (F i size)
+                      $ Seq.update ix (E $ s - size) front
 
 checksum :: Seq Disk -> Int
 checksum = snd . foldl' f (0, 0) . toList
   where
-    f (i, total) (E s) = (i + s, total)
-    f (i, total) (F ident size) = 
-      let total' = sum $ map (* ident) $ take size [i..]
-       in (i + size, total + total')
+    f (i, total) = \case
+       E size       -> (i + size, total)
+       F ident size -> (i + size, total + total')
+         where total' = sum $ map (* ident) $ take size [i..]
 
 main :: IO ()
 main = do
@@ -56,12 +57,9 @@ main = do
   print $ checksum $ moveWhole input
 
 parseInput :: String -> Seq Disk
-parseInput = Seq.fromList . go files . map (read . pure) . init
-  where
-    files = map F [0..]
-    go (f:fs) [] = []
-    go (f:fs) [x] = [f x]
-    go (f:fs) (x:y:xs) = f x : E y : go fs xs
+parseInput = Seq.fromList
+           . zipWith ($) (intersperse E $ map F [0..])
+           . map (read . pure)
 
 -- 6340197768906
 -- 6363913128533
