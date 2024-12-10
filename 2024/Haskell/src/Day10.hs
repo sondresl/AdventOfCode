cfde17cad2d962b5d85e16f99724355ed95aa387
Sondre Lunde <sondrelunde@Sondre-sin-MacBook-Pro.local>
2024-12-10 15:05:58 +0100

    Improve day 10

--- 2024/Haskell/src/Day10.hs
+++ 2024/Haskell/src/Day10.hs
@@ -5,32 +5,21 @@ import Advent.Coord (Coord)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Semigroup
 
--- 'Smart' solution that is just as fast/slow as the dumb dfs with
--- backtracking. This uses dynamic programming to 'cache' the result at every
--- point, and combining these caches at intersections so the final results are
--- in every start node.
-general :: Map Coord Int -> Map Coord (Map Coord Int)
-general input = flip Map.restrictKeys (Set.fromList starts) $ foldl go ends starts
+solve :: Monoid m => Map Coord Int -> (Coord -> m) -> Coord -> m
+solve input f = go 0
   where
-    ends = Map.unions [ Map.singleton pos (Map.singleton pos 1) | (pos, 9) <- Map.assocs input ]
-    starts = [ pos | (pos, 0) <- Map.assocs input ]
-    go seen pos = foldr f (Map.insert pos Map.empty seen) next
-      where
-        val = input Map.! pos
-        next = filter ((== Just (val + 1)) . (`Map.lookup` input))
-             $ ordinalNeighbours pos
-        f new seen = case Map.lookup new seen of
-          Just i -> Map.insertWith (Map.unionWith (+)) pos i seen
-          Nothing -> let seen' = go seen new
-                      in Map.insertWith (Map.unionWith (+)) pos (seen' Map.! new) seen'
+    go 9 pos = f pos
+    go v pos = foldMap (go (v + 1)) next
+      where next = [ p | p <- ordinalNeighbours pos, Map.lookup p input == Just (v + 1)]
 
 main :: IO ()
 main = do
   input <- parseAsciiMap (Just . read . pure) <$> readFile "../data/day10.in"
-  let result = general input
-  print $ sum $ Map.size <$> result
-  print $ sum $ sum      <$> result
+  let starts = [ k | (k, 0) <- Map.assocs input ]
+  print $ sum $ map (Set.size . solve input Set.singleton  ) starts
+  print $ sum $ map (getSum   . solve input (const (Sum 1))) starts
 
 -- 811
 -- 1794
