8bfc0e43ed7aabc865e7b6c69667ccf8c39e40dd
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:18 +0100

    Added type for instruction container

--- 2020/Haskell/src/Day08.hs
+++ 2020/Haskell/src/Day08.hs
@@ -1,5 +1,6 @@
 module Day08 where
 
+import Control.Comonad.Store (peeks)
 import Control.Lens
 import Data.Char (toUpper)
 import Data.List (find)
@@ -7,25 +8,26 @@ import Data.Maybe (mapMaybe)
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import Lib (firstRepeatOn)
-import Control.Comonad.Store ( peeks )
 
 data Instruction = Nop Int | Jmp Int | Acc Int
     deriving (Show, Eq, Ord, Read)
 makePrisms ''Instruction
 
+type Instructions = Vector Instruction
+
 data Program = Program
-    { _instructions :: Vector Instruction
+    { _instructions :: Instructions
     , _ip :: Int
     , _acc :: Int
     }
     deriving (Show)
 makeLenses ''Program
 
-mkProgram :: [Instruction] -> Program
-mkProgram is = Program (Vec.fromList is) 0 0
+mkProgram :: Instructions -> Program
+mkProgram is = Program is 0 0
 
-parseInput :: String -> [Instruction]
-parseInput = map (read . over _head toUpper) . lines . filter (/= '+')
+parseInput :: String -> Instructions
+parseInput = Vec.fromList . map (read . over _head toUpper) . lines . filter (/= '+')
 
 run :: Program -> Program
 run p@Program{..} = case _instructions ^? ix _ip of
@@ -34,17 +36,17 @@ run p@Program{..} = case _instructions ^? ix _ip of
     Just (Acc i) -> p & ip +~ 1 & acc +~ i
     Nothing -> p
 
-changeInstructions :: [Instruction] -> [[Instruction]]
-changeInstructions input = map (peeks changeInst) (holesOf (traverse . filtered (hasn't _Acc))  input)
+changeInstructions :: Instructions -> [Instructions]
+changeInstructions = map (peeks changeInst) . holesOf (traverse . filtered (hasn't _Acc))
   where
     changeInst (Jmp i) = Nop i
     changeInst (Nop i) = Jmp i
     changeInst n = n
 
-part1 :: [Instruction] -> Maybe Int
+part1 :: Instructions -> Maybe Int
 part1 = fmap _acc . firstRepeatOn _ip . iterate run . mkProgram
 
-part2 :: [Instruction] -> Maybe Int
+part2 :: Instructions -> Maybe Int
 part2 = fmap _acc . find ((== 647) . _ip) . mapMaybe check . changeInstructions
   where
     check = firstRepeatOn _ip . iterate run . mkProgram
