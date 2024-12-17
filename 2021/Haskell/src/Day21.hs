aa59f21f3453e99f4c7c9303582cf7586a370ff3
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:51 +0100

    Improve day 21

--- 2021/Haskell/src/Day21.hs
+++ 2021/Haskell/src/Day21.hs
@@ -1,41 +1,46 @@
 module Day21 where
 
-import Lib (tuple, freqs)
+import Lib (tuple, freqs, (.:))
 import Control.Applicative (liftA3)
-import Data.List.Extra (sortBy)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Semigroup (Sum(..))
 
-play :: ((Int, Int), (Int, Int)) -> Int -> [Int] -> Int
-play ((score1, pos1), (score2, pos2)) cnt rolls =
+type Frequencies = Map (Int, Int, Int, Int) Int
+
+play :: (Int, Int) -> (Int, Int) -> Int -> [Int] -> Int
+play (score1, pos1) (score2, pos2) cnt rolls =
   let turn = take 3 rolls
       pos' = (pos1 + sum turn) `mod` 10
       score' = score1 + pos' + 1
    in if score' >= 1000
          then score2 * (cnt + 3)
-         else play ((score2, pos2), (score', pos')) (cnt + 3) (drop 3 rolls)
+         else play (score2, pos2) (score', pos') (cnt + 3) (drop 3 rolls)
 
-outcomes :: Int -> Int -> Map Int (Int, Int) -> [(Int, Int)]
-outcomes turn worlds mp =
-  let fr = sortBy (flip compare) . Map.toList . freqs $ liftA3 (\a b c -> a + b + c) [1,2,3] [1,2,3] [1,2,3]
-   in do
-     (val, freq) <- fr
-     let (score, pos) = mp Map.! turn
-         pos' = (pos + val) `mod` 10
-         score' =  score + pos' + 1
-     if score' >= 21 
-        then pure (turn, worlds * freq)
-        else outcomes ((turn + 1) `mod` 2) (worlds * freq) (Map.insert turn (score', pos') mp)
+rolls :: [(Int, Int)]
+rolls = Map.toList . freqs $ liftA3 (\a b c -> a + b + c) [1,2,3] [1,2,3] [1,2,3]
+
+solve :: Frequencies -> Int -> Int
+solve mp n = if Map.null unfin
+                then getSum . uncurry max $ Map.foldMapWithKey findScores fin
+                else solve (Map.unionWith (+) res fin) ((n + 1) `mod` 2)
+  where
+    done (_, a, _, b) _ = a >= 21 || b >= 21
+    findScores (_, a, _, b) s = if a > b then (Sum s, 0) else (0, Sum s)
+    res = Map.unionsWith (+) $ map (uncurry once) $ Map.toList unfin
+    (fin, unfin) = Map.partitionWithKey done mp
+    once start@(p1, s1, p2, s2) freq = Map.fromList $ do
+      (v, freq') <- rolls
+      let (pos, score, new) = case n of
+                            0 -> ((p1 + v) `mod` 10, s1 + pos + 1, \a b -> (a, b, p2, s2))
+                            1 -> ((p2 + v) `mod` 10, s2 + pos + 1, \a b -> (p1, s1, a, b))
+      pure $ (new pos score, freq * freq')
 
 main :: IO ()
 main = do
   (one, two) <- parseInput <$> readFile "../data/day21.in"
-  -- let (one, two) = (6,3)
-  print $ (one, two)
-  print $ play ((0, one - 1), (0, two - 1)) 0 (cycle [1..100])
-  print . Map.fromListWith (+) 
-        . outcomes 0 1 
-        $ Map.fromList [(0, (0, one - 1)), (1, (0, two - 1))]
+  print $ play (0, one - 1) (0, two - 1) 0 (cycle [1..100])
+  print $ solve (Map.singleton (one - 1, 0, two - 1, 0) 1) 0
 
 parseInput :: String -> (Int, Int)
 parseInput = tuple . map (read . pure . last) . lines
