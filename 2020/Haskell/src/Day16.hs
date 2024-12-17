e88e25942b84d8a3b3000bb4681068184ff7c9ad
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Changed determine values function to StateT with list

--- 2020/Haskell/src/Day16.hs
+++ 2020/Haskell/src/Day16.hs
@@ -1,15 +1,21 @@
 module Day16 where
 
-import Lib
-import Data.Either ( rights, lefts )
-import Data.List.Extra
-import Text.ParserCombinators.Parsec
+import Control.Monad.State (
+    MonadState (get),
+    MonadTrans (lift),
+    evalStateT,
+    modify,
+ )
+import Data.Bifunctor (Bifunctor (second))
+import Data.Either (lefts, rights)
+import Data.List.Extra (isPrefixOf, sortOn, splitOn, transpose)
+import Data.Map (Map)
 import qualified Data.Map as Map
-import           Data.Map   ( Map )
+import Data.Set (Set)
 import qualified Data.Set as Set
-import           Data.Set   ( Set )
-import Data.Bifunctor
-import Data.Tuple (swap)
+import Data.Traversable (for)
+import Lib (commaNums)
+import Text.ParserCombinators.Parsec
 
 parseInput :: String -> (Rules, [Int], [[Int]])
 parseInput input = (parsedRules, parsedTicket, parsedNearby)
@@ -21,19 +27,19 @@ parseInput input = (parsedRules, parsedTicket, parsedNearby)
     tuple = (,) <$> (read <$> many1 digit) <*> (char '-' *> (read <$> many1 digit))
     parsedRules = either (error . show) Map.unions . traverse (parse parseRules "") . lines $ rules
     parseRules = do
-      name <- manyTill anyChar (string ": ")
-      a <- tuple
-      string " or "
-      b <- tuple
-      pure $ Map.singleton name (a, b)
+        name <- manyTill anyChar (string ": ")
+        a <- tuple
+        string " or "
+        b <- tuple
+        pure $ Map.singleton name (a, b)
 
 type Rule = String
 type Rules = Map Rule ((Int, Int), (Int, Int))
 
 validTicket :: Rules -> [Int] -> Either Int [Int]
 validTicket (Map.elems -> rules) t = case filter (not . isValid rules) t of
-                                       [] -> Right t
-                                       xs -> Left $ sum xs
+    [] -> Right t
+    xs -> Left $ sum xs
   where
     isValid ts x = any (inRange x) ts
 
@@ -44,23 +50,31 @@ createCandidates :: Rules -> [[Int]] -> Map Int (Set Rule)
 createCandidates input = Map.unionsWith Set.intersection . map (isCand input)
   where
     isCand (Map.toList -> rules) xs =
-      Map.fromList . zip [0..] . map Set.unions . transpose $ map (fitsRule xs) rules
+        Map.fromList . zip [0 ..] . map Set.unions . transpose $ map (fitsRule xs) rules
     fitsRule xs (name, cs) =
-      map (\x -> if inRange x cs
+        map
+            ( \x ->
+                if inRange x cs
                     then Set.singleton name
-                    else Set.empty) xs
-
-determineRules :: Map Int (Set Rule) -> [(Rule, Int)]
-determineRules (Map.toList -> nearby) =
-  let xs = sortOn (length . snd) nearby
-   in map (swap . second Set.findMin) $ removeDetermined xs
+                    else Set.empty
+            )
+            xs
 
 removeDetermined :: [(Int, Set Rule)] -> [(Int, Set Rule)]
 removeDetermined [] = []
-removeDetermined ((x,s):xs) =
-  case Set.toList s of
-    [v] -> (x,s) : removeDetermined (sortOn (length . snd) (map (second $ Set.filter (/= v)) xs))
-    _ -> (x,s) : xs
+removeDetermined ((x, s) : xs) =
+    case Set.toList s of
+        [v] -> (x, s) : removeDetermined (sortOn (length . snd) (map (second $ Set.filter (/= v)) xs))
+        _ -> (x, s) : xs
+
+determineRules :: Map Int (Set Rule) -> [(Rule, Int)]
+determineRules (Map.toList -> nearby) =
+    head . flip evalStateT Set.empty $ do
+        for (sortOn (Set.size . snd) nearby) $ \(i, cands) -> do
+            seen <- get
+            pick <- lift . Set.toList $ cands Set.\\ seen
+            modify $ Set.insert pick
+            pure (pick, i)
 
 part1 :: Rules -> [[Int]] -> Int
 part1 input xs = sum . lefts $ map (validTicket input) xs
@@ -72,10 +86,10 @@ part2 rules valids ticket = product . map ((ticket !!) . snd) . filter (isPrefix
 
 main :: IO ()
 main = do
-  (rules, ticket, nearby) <- parseInput <$> readFile "../data/day16.in"
-  print $ part1 rules nearby
-  let validtickets = rights $ map (validTicket rules) nearby
-  print $ part2 rules validtickets ticket
+    (rules, ticket, nearby) <- parseInput <$> readFile "../data/day16.in"
+    print $ part1 rules nearby
+    let validtickets = rights $ map (validTicket rules) nearby
+    print $ part2 rules validtickets ticket
 
 -- 22977
 -- 998358379943
