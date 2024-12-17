2f8f7b00a21d784c1c32124429d7dc80684663b1
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:17 +0100

    Day 12 2019

--- 2019/Haskell/Day12.hs
+++ 2019/Haskell/Day12.hs
@@ -1,10 +1,11 @@
 import Data.List
+import qualified Data.Set as S
 import Data.Tuple.Extra
 
 import Debug.Trace
 
-type Pos = [Int]
-type Vel = [Int]
+type Pos = [Integer]
+type Vel = [Integer]
 type Moon = (Pos, Vel) 
  
 initialize :: [Pos] -> [Moon]
@@ -21,29 +22,45 @@ newVelocity xs (we, sp) =
       diff = map sum . transpose $ map (zipWith gravity we) moons
    in (we, zipWith (+) sp diff)
 
-gravity :: Int -> Int -> Int
+gravity :: Integer -> Integer -> Integer
 gravity a b 
   | a < b = 1 
   | a > b = (-1) 
   | a == b = 0
 
-period :: [Moon] -> Moon -> Int
-period moons moon = (+1) . length . takeWhile (all (/= moon)) . iterate step . step $ moons
+compVar :: Int -> Moon -> Moon -> Bool
+compVar n self tar = (fst self !! n) == (fst tar !! n) && (snd self !! n) == (snd tar !! n)
 
-solveA :: [Moon] -> Int
+findFirst :: S.Set Integer -> [Integer] -> Integer
+findFirst set (x:xs)
+  | S.member (div x 2) set = div x 2
+  | otherwise = findFirst (S.insert x set) xs
+
+period :: [Moon] -> Integer -> Integer
+period moons n = 
+  let moon = moons !! (fromInteger n)
+      matchingStates co = co moon . (!! (fromInteger n)) . snd
+      ixs co = findFirst S.empty . map fst . filter (matchingStates co) . zip [1..] . iterate step . step $ moons
+      x = ixs (compVar 0)
+      y = ixs (compVar 1)
+      z = ixs (compVar 2)
+   in foldr lcm 1 [x,y,z]
+
+solveA :: [Moon] -> Integer
 solveA = sum . map energy . last . take 1000 . iterate step . step
   where energy moon = uncurry (*) . both (sum . map abs) $ moon
 
-solveB :: [Moon] -> Int
-solveB moons = foldr1 lcm . map (period moons) $ moons
+solveB :: [Moon] -> Integer
+solveB moons = 
+  let a = map (period moons) $ [0..3]
+   in foldr1 lcm a
 
 main = do
   input <- pure $ initialize [[-7, -1, 6], [6, -9, -9], [-12, 2, -7], [4, -17, -12]]
-  test <- pure $ initialize [[-1, 0, 2], [2, -10, -7], [4, -8, 8], [3, 5, -1]]
-  test2 <- pure $ initialize [[-8, -10, 0], [5, 5,10], [2, -7, 3], [9, -8, -3]]
-
-  print $ solveA input
-
+  test  <- pure $ initialize [[-1, 0, 2], [2, -10, -7], [4, -8, 8], [3, 5, -1]]
   print $ solveB test
+  print $ solveB input
 
 -- 11384
+-- 452582583272768
+
