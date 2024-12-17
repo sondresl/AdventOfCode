12770142b42fbfa5d8b5b0ae0a8fd0db790d0fc7
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:53 +0100

    Day 9 with reverse

--- 2023/Haskell/src/Day09.hs
+++ 2023/Haskell/src/Day09.hs
@@ -2,14 +2,16 @@ module Day09 where
 
 import Lib (allNums, takeUntil)
 
-solve :: ([Int] -> Int -> Int) -> [Int] -> Int
-solve f = foldr f 0 . takeUntil (all (== 0)) . iterate (map (uncurry subtract) . (zip <*> tail))
+solve :: [Int] -> Int
+solve = foldr ((-) . head) 0 
+      . takeUntil (all (== 0)) 
+      . iterate (map (uncurry subtract) . (zip <*> tail))
 
 main :: IO ()
 main = do
   input <- map allNums . lines <$> readFile "../data/day09.in"
-  print $ sum $ map (solve ((+) . last)) input
-  print $ sum $ map (solve ((-) . head)) input
+  print $ sum $ map (solve . reverse) input
+  print $ sum $ map solve input
 
 -- 1939607039
 -- 1041
