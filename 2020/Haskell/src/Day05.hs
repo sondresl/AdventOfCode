a3bd1213b4e4db2fbc9cb3ef785e4454063fef14
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:18 +0100

    Day 5 solution

--- 2020/Haskell/src/Day05.hs
+++ 2020/Haskell/src/Day05.hs
@@ -1,18 +1,31 @@
 module Day05 where
 
-import Lib
-import Data.Maybe
-import Data.List.Extra
+import Data.List (find, sort)
+import Data.Digits ( unDigits )
 
-parseInput = id
+seat :: [Char] -> Int
+seat = unDigits 2 . map f
+  where f 'F' = 0
+        f 'B' = 1
+        f 'L' = 0
+        f 'R' = 1
 
-part1 input = undefined
+part1 :: [String] -> Int
+part1 = maximum . map seat
 
-part2 input = undefined
+part2 :: [String] -> Maybe Int
+part2 =
+    fmap snd
+        . find (uncurry (/=))
+        . (zip <*> (tail . map pred))
+        . sort
+        . map seat
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "../data/day05.in"
-  print input
-  -- print $ part1 input
-  -- print $ part2 input
+    input <- lines <$> readFile "../data/day05.in"
+    print $ part1 input
+    print $ part2 input
+
+-- 883
+-- 532
