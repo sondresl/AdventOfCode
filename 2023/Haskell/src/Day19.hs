0fd38343d593cc78727b8ee09155dff4a3a534a5
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:53 +0100

    Switch to V4

--- 2023/Haskell/src/Day19.hs
+++ 2023/Haskell/src/Day19.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Day19 where
 
-import Lib (tuple, allNums)
+import Lib ((.:), tuple, allNums)
 import Data.List.Extra (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -8,109 +9,56 @@ import Text.ParserCombinators.Parsec
     (parse, many1, letter, between, char, try, (<|>), sepBy1, oneOf, digit, Parser)
 import qualified Data.Interval as I
 import Data.Char (toUpper)
+import Linear (V4(..))
+import Control.Lens.TH (makeLenses)
+import Control.Lens (set, (&), (.~), Field1(_1), Field2(_2), Field3(_3), Field4(_4))
 
-data Status = Accepted | Rejected
-  deriving (Show, Eq, Ord)
+type Rating = V4 Int
+type Work = V4 (I.Interval Int)
+data WF = WF Work Work String
+  deriving Show
 
-data Category = X | M | A | S
-  deriving (Read, Show, Eq, Ord)
+defaultInterval :: Work
+defaultInterval = pure $ 1 I.<=..<= 4000
 
-lookupCat :: Rating a -> Category -> a
-lookupCat rating = \case
-  X -> x rating
-  M -> m rating
-  A -> a rating
-  S -> s rating
-
-updateRating :: Rating a -> Category -> a -> Rating a
-updateRating rating cat val = 
-  case cat of
-    X -> rating { x = val }
-    M -> rating { m = val }
-    A -> rating { a = val }
-    S -> rating { s = val }
-
-data Rating a = Rating
-  { x :: a
-  , m :: a
-  , a :: a
-  , s :: a
-  } deriving (Show, Eq, Ord)
-
-defaultInterval :: Rating (I.Interval Int)
-defaultInterval = Rating i i i i
-  where i = 1 I.<=..<= 4000
-
-data Workflow = Workflow
-  { test :: (Category, I.Interval Int)
-  , dest :: String
-  } deriving Show
-
-type Workflows = Map String [Workflow]
-
-part1 :: Workflows -> [Rating Int] -> Int
-part1 flows = sum . map sumRating . filter ((==Accepted) . runFlow flows)
-  where sumRating (Rating x m a s) = x + m + a + s
-
-reduceWorkflow :: Map String [Workflow] -> [Rating (I.Interval Int)]
-reduceWorkflow flows = go defaultInterval (flows Map.! "in")
+reduceWF :: Map String [WF] -> [Work]
+reduceWF input = go defaultInterval (input Map.! "in")
   where
-    go ivs [Workflow _ "A"] = [ivs]
-    go ivs [Workflow _ "R"] = []
-    go ivs [Workflow _ pattern] = go ivs (flows Map.! pattern)
-    go ivs ((Workflow (cat, iv) dest):next) = passTest <> go failRating next
-      where
-        oldIv = lookupCat ivs cat
-        passTest = case dest of
-          "A" -> [successRating] 
-          "R" -> []
-          _ -> go successRating (flows Map.! dest)
-        successRating = updateRating ivs cat (I.intersection iv oldIv)
-        failRating = updateRating ivs cat failIv
-        failIv = if I.lowerBound iv == I.Finite 1
-                    then I.intersection ((I.upperBound iv + 1) I.<=..<= 4000) oldIv
-                    else I.intersection (1 I.<=..<= subtract 1 (I.lowerBound iv)) oldIv
+    go _   []                      = []
+    go rating (WF pass fail "A":wfs)  = [combine rating pass]                       <> go (combine rating fail) wfs
+    go rating (WF pass fail "R":wfs)  = []                                          <> go (combine rating fail) wfs
+    go rating (WF pass fail dest:wfs) = go (combine rating pass) (input Map.! dest) <> go (combine rating fail) wfs
+    combine = liftA2 I.intersection
 
-runFlow :: Workflows -> Rating Int -> Status
-runFlow ws rating = go (ws Map.! "in")
-  where
-    go ((Workflow (cat, iv) dest):wf)
-      | lookupCat rating cat `I.member` iv = next dest
-      | otherwise = go wf
-      where
-        next "A" = Accepted
-        next "R" = Rejected
-        next xs = go (ws Map.! dest)
-
-countPerms :: Rating (I.Interval Int) -> Int
-countPerms (Rating x m a s) = product $ map (succ . I.width) [x,m,a,s]
+accept :: [Work] -> Rating -> Bool
+accept works rat = any (and . liftA2 I.member rat) works
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "../data/day19.in"
-  let (ratings, flows) = input
-      reduced = reduceWorkflow flows
-  print $ part1 flows ratings
-  print . sum $ map countPerms reduced
+  (ratings, flows) <- parseInput <$> readFile "../data/day19.in"
+  let reduced = reduceWF flows
+  print . sum . map sum $ filter (accept reduced) ratings
+  print . sum $ map (product . fmap (succ . I.width)) reduced
 
-parseInput :: String -> ([Rating Int], Map String [Workflow])
+parseInput :: String -> ([Rating], Map String [WF])
 parseInput input = (ratings', Map.fromList $ map parseFlow flows)
   where
     (flows, ratings) = tuple . map lines $ splitOn "\n\n" input
-    ratings' = map ((\[x,m,a,s] -> Rating x m a s) . allNums) ratings
-    parseFlow :: String -> (String, [Workflow])
+    ratings' = map ((\[x,m,a,s] -> V4 x m a s) . allNums) ratings
     parseFlow = either (error . show) id . parse p ""
     p = (,) <$> many1 letter
             <*> between (char '{') (char '}') ((try parseWork <|> parseLetters) `sepBy1` char ',')
     parseWork = do
-      rat <- read . map toUpper <$> many1 letter
+      rat <- map toUpper <$> many1 letter
       comp <- oneOf "<>"
       n <- fromIntegral . read @Int <$> (many1 digit <* char ':')
       dest <- many1 letter
-      let fn = if comp == '>' then (n + 1) I.<=..<= 4000 else 1 I.<=..<= (n - 1)
-      pure $ Workflow (rat, fn) dest
-    -- Just pick a Category and make it unrestricted
-    parseLetters = Workflow (X, 1 I.<=..<= 4000) <$> many1 letter
+      let f :: I.Interval Int -> Work
+          f i = set (case rat of "X" -> _1; "M" -> _2; "A" -> _3; "S" -> _4) i defaultInterval
+      pure $ if comp == '>' 
+                 then WF (f $ (n + 1) I.<=..<= 4000)    (f $ 1 I.<=..<= n   ) dest
+                 else WF (f $ 1       I.<=..<= (n - 1)) (f $ n I.<=..<= 4000) dest
+    parseLetters = WF defaultInterval defaultInterval <$> many1 letter
 
 -- 332145
 -- 136661579897555
