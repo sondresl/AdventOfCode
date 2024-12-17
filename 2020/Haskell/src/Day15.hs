57a58f929e363c5872689d6bd9e3b48594889a85
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    -> Unboxed vectors, slight refactoring

--- 2020/Haskell/src/Day15.hs
+++ 2020/Haskell/src/Day15.hs
@@ -1,56 +1,34 @@
 module Day15 where
 
-import qualified Data.Vector.Mutable as MV
-import Control.Monad.ST ( ST, runST )
+import Control.Monad.ST (runST)
+import qualified Data.Vector.Unboxed.Mutable as MV
+import Control.Monad (forM_)
 
-import Data.IntMap (IntMap)
-import qualified Data.IntMap.Strict as Map
-import Control.Monad (foldM)
+run :: Int -> [Int] -> Int
+run target xs = runST $ do
+    vec <- MV.replicate (target + 1) 0
 
-run :: Int -> [Int] -> ST s Int
-run target xs = do
-  vec <- MV.new (target + 1)
-  MV.set vec 0
-  vec <- foldM initial vec (zip [1..] (init xs))
-  runMut vec (last xs) 6
-    where
-      initial vec (i, x) = do
+    forM_ (zip [1 ..] (init xs)) $ \(i, x) -> do
         MV.write vec x i
-        pure vec
-      runMut vec current turn = do
-        prev <- MV.read vec current
-        let next = if prev == 0
-                     then 0
-                     else turn - prev
-        MV.write vec current turn
-        if turn == target
-           then pure current
-           else runMut vec next (turn + 1)
 
-play :: Int -> IntMap Int -> Int -> Int -> Int
-play target seen turn current = go seen turn current
-  where
-    go seen !turn !current
-      | turn == target = current
-      | otherwise =
-          go (Map.insert current turn seen)
-             (succ turn)
-             (case Map.lookup current seen of
-                Nothing -> 0
-                Just prev -> turn - prev
-             )
+    let runMut current turn
+          | turn == target = pure current
+          | otherwise = do
+              next <- MV.read vec current >>= \case
+                        0    -> pure 0
+                        prev -> pure $ turn - prev
+              MV.write vec current turn
+              if turn == target
+                  then pure current
+                  else runMut next (turn + 1)
 
-initialize :: Int -> [Int] -> Int
-initialize n xs = play n (Map.fromList (zip (init xs) [1 ..])) (length xs) (last xs)
+    runMut (last xs) (length xs)
 
 main :: IO ()
 main = do
     let input = [1, 20, 11, 6, 12, 0]
-    -- print $ initialize 2020 input
-    -- print $ initialize 30000000 input
-
-    print $ runST (run 2020 input)
-    print $ runST (run 30000000 input)
+    print $ run 2020 input
+    print $ run 30000000 input
 
 -- 1085
 -- 10652
