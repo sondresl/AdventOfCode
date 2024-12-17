20217c62a3c849baa613fd01a4eb8c8bdd61ca55
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:54 +0100

    day 3 + readme stuff + bench

--- 2024/Haskell/README.md
+++ 2024/Haskell/README.md
@@ -1,1 +1,23 @@
-# aoc2024
+# Advent of Code 2024
+
+## Day 1
+
+```
+  Time (mean ± σ):      55.0 ms ±   4.2 ms    [User: 15.3 ms, System: 34.0 ms]
+  Range (min … max):    51.6 ms …  73.4 ms    44 runs
+```
+
+## Day 2
+
+```
+  Time (mean ± σ):      53.1 ms ±   4.1 ms    [User: 20.0 ms, System: 35.9 ms]
+  Range (min … max):    48.6 ms …  73.2 ms    45 runs
+```
+
+## Day 3
+
+```
+  Time (mean ± σ):      56.7 ms ±   5.9 ms    [User: 22.4 ms, System: 37.4 ms]
+  Range (min … max):    48.4 ms …  69.7 ms    44 runs
+```
+
--- 2024/Haskell/src/Advent/Parsers.hs
+++ 2024/Haskell/src/Advent/Parsers.hs
@@ -7,3 +7,15 @@ pNumber = read <$> (try ((:) <$> char '-' <*> many1 digit) <|> many1 digit)
 
 pBetween :: String -> String -> Parser a -> Parser a
 pBetween start end = between (string start) (string end)
+
+pFind :: Parser a -> Parser a
+pFind = try . pSkipManyTill anyChar
+
+-- https://hackage.haskell.org/package/parser-combinators-1.3.0/docs/src/Control.Monad.Combinators.html#skipManyTill
+pSkipManyTill p end = go
+  where
+    go = do
+      r <- optionMaybe $ try end
+      case r of
+        Nothing -> p >> go
+        Just r -> pure r
