2aa67ed47535f2562c45057632a18454f4a3507b
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:16 +0100

    Removed qualified import for Vector

--- 2019/Haskell/Day02.hs
+++ 2019/Haskell/Day02.hs
@@ -1,31 +1,31 @@
 import Data.List
-import qualified Data.Vector as V
+import Data.Vector (Vector, (//), (!), fromList)
 import Data.Bool
 
 -- I preprossesed the file to remove commas and allow for simple splitting
 -- using 'words'
 
-strToInts :: String -> V.Vector Int
-strToInts = V.fromList . map read . words
+strToInts :: String -> Vector Int
+strToInts = fromList . map read . words
 
-compute :: Int -> V.Vector Int -> Int
+compute :: Int -> Vector Int -> Int
 compute i vec
-  | vec V.! i == 99 = vec V.! 0
+  | vec ! i == 99 = vec ! 0
   | otherwise =
-    let code = bool (*) (+) (vec V.! i == 1)
-        a = vec V.! (vec V.! (i + 1))
-        b = vec V.! (vec V.! (i + 2))
-        c = vec V.! (i + 3)
+    let code = bool (*) (+) (vec ! i == 1)
+        a = vec ! (vec ! (i + 1))
+        b = vec ! (vec ! (i + 2))
+        c = vec ! (i + 3)
         new = code a b
-     in compute (i + 4) (V.unsafeUpd vec [(c, new)])
+     in compute (i + 4) (vec // [(c, new)])
 
-solveA :: V.Vector Int -> Int
-solveA = compute 0 . flip V.unsafeUpd [(1, 12), (2, 2)]
+solveA :: Vector Int -> Int
+solveA = compute 0 . (// [(1, 12), (2, 2)])
 
-solveB :: V.Vector Int -> Int
+solveB :: Vector Int -> Int
 solveB vec =
   let xs = [[i, j] | i <- [0..99], j <- [0..99]]
-      f [a,b] = compute 0 (V.unsafeUpd vec [(1, a), (2, b)]) == 19690720
+      f [a,b] = compute 0 (vec // [(1, a), (2, b)]) == 19690720
       Just (a:b:_) = find f xs
    in 100 * a + b
 
