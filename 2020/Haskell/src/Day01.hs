6796e6defa523a2efea6e3f26f80223b85b4c637
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:18 +0100

    Some cleanup

--- 2020/Haskell/src/Day01.hs
+++ 2020/Haskell/src/Day01.hs
@@ -1,20 +1,26 @@
 module Day01 where
 
-import Lib ( linedNums )
-import Data.Maybe ( listToMaybe )
+import Lib (linedNums)
+import Data.Maybe (listToMaybe)
 
 part1 :: [Int] -> Maybe Int
-part1 input = listToMaybe [x * y
-                          | x <- input
-                          , y <- input
-                          , x + y  == 2020]
+part1 input =
+  listToMaybe
+    [ x * y
+    | x <- input
+    , y <- input
+    , x + y == 2020
+    ]
 
 part2 :: [Int] -> Maybe Int
-part2 input = listToMaybe [x * y * z
-                          | x <- input
-                          , y <- input
-                          , z <- input
-                          , x + y + z == 2020]
+part2 input =
+  listToMaybe
+    [ x * y * z
+    | x <- input
+    , y <- input
+    , z <- input
+    , x + y + z == 2020
+    ]
 
 main :: IO ()
 main = do
