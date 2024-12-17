874c63ffa7a6d4dbd00bea9a626734f924f739d9
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Much improved parsing

--- 2020/Haskell/src/Day19.hs
+++ 2020/Haskell/src/Day19.hs
@@ -4,32 +4,28 @@ import Control.Monad (foldM, guard)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as Map
 import Data.List.Extra ( splitOn )
-import Lib (count)
+import Lib (count, tuple)
 import Text.ParserCombinators.Parsec
   (char, digit, letter, many1, parse, sepBy, sepEndBy, space, string, (<|>),)
+import Data.Bifunctor (bimap)
 
 type Rule = Either Char [[Int]]
 
 parseInput :: String -> (IntMap Rule, [String])
-parseInput = (\[a, b] -> (p a, lines b)) . splitOn "\n\n"
+parseInput = bimap rules lines . tuple . splitOn "\n\n"
   where
-    p :: String -> IntMap Rule
-    p = Map.unions . map (either (error . show) id . parse parseRules "") . lines
+    rules = Map.unions . map (either (error . show) id . parse parseRules "") . lines
     direct = Left <$> (char '"' *> letter <* char '"')
-    parseRules = do
-        i <- read <$> many1 digit
-        string ": "
-        rs <- sepBy (sepEndBy (direct <|> (Right . read <$> many1 digit)) space) (string "| ")
-        let rs' = case rs of
-                [[Left c]] -> Left c
-                xs -> mapM sequence xs
-        pure $ Map.singleton i rs'
+    parseRules =
+        Map.singleton
+          <$> (read <$> many1 digit <* string ": ")
+          <*> (direct <|> Right <$> sepBy (sepEndBy (read <$> many1 digit) space) (string "| "))
 
 expand :: IntMap Rule -> Rule -> String -> [String]
 expand _ _ [] = []
 expand rules rule (s : str) = case rule of
     Left c -> guard (c == s) >> pure str
-    Right xs -> concatMap (foldM (\acc x -> expand rules (rules Map.! x) acc) (s : str)) xs
+    Right xs -> xs >>= foldM (\acc x -> expand rules (rules Map.! x) acc) (s : str)
 
 part1 :: IntMap Rule -> [String] -> Int
 part1 rules = count (any null) . map (expand rules (Right [[0]]))
@@ -39,7 +35,7 @@ part2 rules = count (any null) . map (expand (newRules <> rules) (Right [[0]]))
   where
     newRules =
         Map.fromList
-            [ (8, Right [[42], [42, 8]])
+            [ (8,  Right [[42    ], [42,  8    ]])
             , (11, Right [[42, 31], [42, 11, 31]])
             ]
 
