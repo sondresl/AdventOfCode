9dd5c3515dda3a3c9ab8e2fd8a0272d530c70e19
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:54 +0100

    Day 11

--- 2024/Haskell/src/Day11.hs
+++ 2024/Haskell/src/Day11.hs
@@ -1,41 +1,30 @@
 module Day11 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
+import Lib (iterateN, allNums)
+import Data.Digits (digits, unDigits)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.RawString.QQ
-import Text.ParserCombinators.Parsec hiding (count)
 
-part1 input = undefined
+blinkStones :: Map Int Int -> Map Int Int
+blinkStones stones = Map.fromListWith (+) $ do
+  (stone, count) <- Map.assocs stones
+  map (,count) $ blink stone
 
-part2 input = undefined
+blink :: Int -> [Int]
+blink 0 = [1]
+blink n 
+  | odd (length digs) = [n * 2024]
+  | otherwise         = [left, right]
+    where
+      digs = digits 10 n
+      left = unDigits 10 $ take (length digs `div` 2) digs
+      right = unDigits 10 $ drop (length digs `div` 2) digs
 
 main :: IO ()
 main = do
+  input <- Map.fromList . map (,1) . allNums <$> readFile "../data/day11.in"
+  print $ sum $ iterateN 25 blinkStones input
+  print $ sum $ iterateN 75 blinkStones input
 
-  let run str input = do
-        putStrLn str
-        print input
-
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" $ parseInput testInput
-
-  -- input <- parseInput <$> readFile "../data/day11.in"
-  -- run "\nActual:\n\n" input
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
-
-testInput = [r|
-|]
+-- 199946
+-- 237994815702032
