335515f4ae5b7fc6b4d332b71de25cd80118df93
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Simplified by merging work functions

--- 2020/Haskell/src/Day12.hs
+++ 2020/Haskell/src/Day12.hs
@@ -1,65 +1,43 @@
 module Day12 where
 
-import Control.Lens ( (&), view, (%~), (+~), (<>~), makeLenses )
-import Lib ( mannDist, Dir(..), Point )
-import Linear (V2 (..), perp)
+import Advent.Coord
+    ( origin, up, down, left, right, turnLeft, turnRight )
+import Control.Lens ( Lens', (&), view, (%~), (+~), makeLenses )
+import Lib ( mannDist, Point )
+import Linear (V2 (..), (*^) )
+import Data.List (foldl')
 
-data Move = N | S | E | W | L | R | F
+data Command = N | S | E | W | L | R | F
     deriving (Show, Read, Eq, Ord)
 
 data Ship = Ship
-    { _dir :: !Dir
+    { _dir :: !Point
     , _pos :: !Point
     }
     deriving (Show, Eq, Ord)
 makeLenses ''Ship
 
-parseInput :: String -> [(Move, Int)]
+parseInput :: String -> [(Command, Int)]
 parseInput = map (\x -> (read [head x], read $ tail x)) . lines
 
-dirToMove North = N
-dirToMove East = E
-dirToMove West = W
-dirToMove South = S
+move :: Lens' Ship (V2 Int) -> Ship -> (Command, Int) -> Ship
+move l ship c = ship & case c of
+  (N, n  ) -> l +~ n *^ up
+  (E, n  ) -> l +~ n *^ right
+  (S, n  ) -> l +~ n *^ down
+  (W, n  ) -> l +~ n *^ left
+  (_, 180) -> dir %~ negate
+  (L, 90 ) -> dir %~ turnLeft
+  (R, 90 ) -> dir %~ turnRight
+  (L, 270) -> dir %~ turnRight
+  (R, 270) -> dir %~ turnLeft
+  (F, n)   -> pos +~ (n *^ view dir ship)
 
-move :: Ship -> (Move, Int) -> Ship
-move ship m = case m of
-  (N, n  ) -> ship & pos +~ V2 0 n
-  (E, n  ) -> ship & pos +~ V2 n 0
-  (S, n  ) -> ship & pos +~ V2 0 (-n)
-  (W, n  ) -> ship & pos +~ V2 (-n) 0
-  (_, 180) -> ship & dir <>~ South
-  (L, 90 ) -> ship & dir <>~ West
-  (R, 90 ) -> ship & dir <>~ East
-  (L, 270) -> ship & dir <>~ East
-  (R, 270) -> ship & dir <>~ West
-  (F, n)   -> move ship (dirToMove (view dir ship), n)
+part1 :: [(Command, Int)] -> Int
+part1 = mannDist (V2 0 0) . view pos . foldl' (move pos) (Ship right origin)
 
-part1 :: [(Move, Int)] -> Int
-part1 = mannDist (V2 0 0) . view pos . foldl move (Ship East (V2 0 0))
-
-data Waypoint = Waypoint
-  { _wp :: Point
-  , _ship :: Point
-  }
-  deriving (Show, Eq, Ord)
-makeLenses ''Waypoint
-
-waypoint :: Waypoint -> (Move, Int) -> Waypoint
-waypoint current m = case m of
-  (N, n  ) -> current & wp +~ V2 0 n
-  (E, n  ) -> current & wp +~ V2 n 0
-  (S, n  ) -> current & wp +~ V2 0 (-n)
-  (W, n  ) -> current & wp +~ V2 (-n) 0
-  (_, 180) -> current & wp %~ negate
-  (L, 90 ) -> current & wp %~ perp
-  (R, 90 ) -> current & wp %~ perp . perp . perp
-  (L, 270) -> waypoint current (R, 90)
-  (R, 270) -> waypoint current (L, 90)
-  (F, n  ) -> current & ship +~ V2 n n * view wp current
-
-part2 :: [(Move, Int)] -> Int
-part2 = mannDist (V2 0 0) . view ship . foldl waypoint (Waypoint (V2 10 1) (V2 0 0))
+part2 :: [(Command, Int)] -> Int
+part2 = mannDist (V2 0 0) . view pos . foldl' (move dir) (Ship (V2 10 1) origin)
 
 main :: IO ()
 main = do
