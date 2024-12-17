5094cb9bc063df15fe9e1ca96cacc90118ab85b2
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Day 15 initial solution

--- 2020/Haskell/src/Day15.hs
+++ 2020/Haskell/src/Day15.hs
@@ -1,19 +1,33 @@
 module Day15 where
 
-import Lib
-import Data.Maybe
-import Data.List.Extra
-import Text.ParserCombinators.Parsec
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as Map
 
-parseInput = id
+play :: IntMap (Either (Int, Int) Int) -> Int -> Int -> [Int]
+play seen turn lastSpoken =
+    case Map.lookup lastSpoken seen of
+        Just (Right _) -> 0 : play (Map.insertWith f 0 (Right turn) seen) (succ turn) 0
+        Just (Left (prev, old)) ->
+            let diff' = prev - old
+             in diff' : play (Map.insertWith f diff' (Right turn) seen) (succ turn) diff'
+  where
+    f (Right new) (Right old) = Left (new, old)
+    f (Right new) (Left (old, _)) = Left (new, old)
 
-part1 input = undefined
+initialize :: [Int] -> [Int]
+initialize xs = (xs ++) $ play (Map.fromList (zip xs (map pure [1 ..]))) (succ (length xs)) (last xs)
 
-part2 input = undefined
+part1 :: [Int] -> Int
+part1 = (!! 2019) . initialize
+
+part2 :: [Int] -> Int
+part2 = (!! 29999999) . initialize
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "../data/day15.in"
-  print input
-  -- print $ part1 input
-  -- print $ part2 input
+    let input = [1, 20, 11, 6, 12, 0]
+    print $ part1 input
+    print $ part2 input
+
+-- 1085
+-- 10652
