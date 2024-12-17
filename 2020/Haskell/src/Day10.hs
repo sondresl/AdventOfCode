90233088d513bff9ffd89e3ed73555f469257104
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Now with paramorphism

--- 2020/Haskell/src/Day10.hs
+++ 2020/Haskell/src/Day10.hs
@@ -1,26 +1,25 @@
 module Day10 where
 
-import Data.List.Extra ( sort )
+import Data.Functor.Foldable ( ListF(Cons, Nil), Recursive(para) )
+import Data.List.Extra (sort)
 import qualified Data.Map as Map
-import Data.Maybe ( listToMaybe )
-import Lib ( freqs, linedNums )
+import Data.Maybe (listToMaybe)
+import Lib (freqs, linedNums)
 
 part1 :: [Int] -> Maybe Int
 part1 = ans . freqs . map (uncurry subtract) . (zip <*> tail)
   where
     ans input = (*) <$> Map.lookup 1 input <*> Map.lookup 3 input
 
-run :: [Int] -> [(Int, Integer)]
-run [] = []
-run (x : xs) =
-    let rec = run xs
-        v = max (f x rec) 1
-     in (x, v) : rec
+run :: [Int] -> [Integer]
+run = para $ \case
+    Nil -> []
+    Cons x (xs, res) -> f x (zip xs res) : res
   where
-    f x = sum . map snd . filter ((<= 3) . subtract x . fst) . take 3
+    f x = max 1 . sum . map snd . filter ((<= 3) . subtract x . fst) . take 3
 
 part2 :: [Int] -> Maybe Integer
-part2 = fmap snd . listToMaybe . run
+part2 = listToMaybe . run
 
 main :: IO ()
 main = do
