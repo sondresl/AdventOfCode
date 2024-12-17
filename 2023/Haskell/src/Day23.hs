5774451b50f44957e327ccf8108c55b8dcbe3ef3
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:53 +0100

    Day 23 better

--- 2023/Haskell/src/Day23.hs
+++ 2023/Haskell/src/Day23.hs
@@ -1,39 +1,26 @@
 module Day23 where
 
-import Lib
-import Advent.Coord
-import Advent.Search hiding (dfs)
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
+import Lib (Point, ordinalNeighbours, findBounds, parseAsciiMap)
+import Advent.Search (bfsOn)
+import Control.Monad (guard)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.RawString.QQ
-import Text.ParserCombinators.Parsec hiding (count)
-import Linear hiding (trace)
+import Linear (V2(V2))
 import qualified Data.Set as Set
 import Data.Set (Set)
 import Data.Tuple.Extra (first)
-import Debug.Trace
 
-part1 input = bfsOn (\(x,y,z) -> (x,y)) [(0, V2 1 0, V2 1 (-1))] nexts
-  where
-    nexts (i, pos, prev) = map (i+1,,pos)
-                         . filter (/= prev)
-                         . filter (notSlope pos) 
-                         . filter (`Map.member` input)
-                         $ ordinalNeighbours pos
-    notSlope pos c = case (c - pos, input Map.! c) of
-      (V2 0 1   , '^') -> False
-      (V2 1 0   , '<') -> False
-      (V2 0 (-1), 'v') -> False
-      (V2 (-1) 0, '>') -> False
-      _ -> True
+type Graph = Map Point [(Int, Point)]
 
-mkGraph :: Map Point Char -> Map Point [(Int, Point)]
-mkGraph input = foldMap steps crossings
+canWalk :: Map Point Char -> Point -> Point -> Bool
+canWalk input from to = (to - from, input Map.! to) `notElem`
+  [(V2 0 1   , '^')
+  ,(V2 1 0   , '<')
+  ,(V2 0 (-1), 'v')
+  ,(V2 (-1) 0, '>')]
+
+mkGraph :: Map Point Char -> (Point -> Point -> Bool) -> Graph
+mkGraph input f = foldMap steps crossings
   where
     (minx,miny,maxx,maxy) = findBounds (Map.keys input)
     crossings = ([V2 1 0,  V2 maxx maxy] <>) . filter ((>2) . length . filter (`Map.member` input) . ordinalNeighbours) 
@@ -41,31 +28,34 @@ mkGraph input = foldMap steps crossings
     steps pos = Map.singleton pos . filter ((`elem` crossings) . snd) . tail $ bfsOn snd [(0,pos)] nexts
      where
       nexts (_, p) | p /= pos && p `elem` crossings = []
-      nexts (i, p) = map (i+1,) . filter (`Map.member` input) 
+      nexts (i, p) = map (i+1,) 
+                   . filter (f pos)
+                   . filter (`Map.member` input) 
                    $ ordinalNeighbours p
 
-dfs :: ((Int, Point) -> [(Int, Point)]) -> Point -> Point -> [(Int, Point)]
-dfs next end start = go Set.empty (0, start)
+solve :: Graph -> Int
+solve input = maximum . map fst $ longestPath nexts end (V2 1 0)
   where
-    go seen x@(i, pos) | pos == end = [x]
-    go seen x@(i, pos)
-      | Set.member pos seen = []
-      | otherwise = do
-          n <- next x
-          let seen' = Set.insert pos seen
-          go seen' n
-
-part2 (mkGraph -> input) = dfs nexts (V2 maxx maxy) (V2 minx miny)
-  where
-    (minx,miny,maxx,maxy) = findBounds (Map.keys input)
+    end = maximum $ Map.keys input
     nexts (i, pos) = map (first (+i)) $ input Map.! pos
+    longestPath next end start = go Set.empty (0, start)
+      where
+        go seen x@(i, pos) | pos == end = [x]
+        go seen x@(i, pos)
+          | Set.member pos seen = []
+          | otherwise = do
+              n <- next x
+              let seen' = Set.insert pos seen
+              go seen' n
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "../data/day23.in"
   let (minx,miny,maxx,maxy) = findBounds (Map.keys input)
-  print $ last $ filter ((== V2 maxx maxy) . view _2) $ part1 input
-  print $ maximumOn fst $ part2 input
+  print $ Map.size (mkGraph input (canWalk input))
+  print $ Map.size $ mkGraph input (const $ const True)
+  -- print $ solve (mkGraph input (canWalk input))
+  -- print $ solve (mkGraph input (const $ const True))
 
 parseInput :: String -> Map Point Char
 parseInput = parseAsciiMap f
