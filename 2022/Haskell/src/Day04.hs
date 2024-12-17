edec47894498a7c1d3e7abf33c7dc7270c966af7
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:52 +0100

    Day 4

--- 2022/Haskell/src/Day04.hs
+++ 2022/Haskell/src/Day04.hs
@@ -1,36 +1,19 @@
 module Day04 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
-
-part1 input = undefined
-
-part2 input = undefined
+import Lib (count)
+import Data.List.Extra (splitOn)
+import Data.IntSet (IntSet, isSubsetOf, disjoint, fromList)
 
 main :: IO ()
 main = do
+  input <- map parseInput . lines <$> readFile "../data/day04.in"
+  print $ count ((||) <$> uncurry isSubsetOf <*> uncurry (flip isSubsetOf)) input
+  print $ count (not . uncurry disjoint) input
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
+parseInput :: String -> (IntSet, IntSet)
+parseInput str = (fromList [a..b], fromList [c..d])
+  where
+    [[a,b], [c,d]] = map (map read . splitOn "-") . splitOn "," $ str
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day04.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 580
+-- 895
