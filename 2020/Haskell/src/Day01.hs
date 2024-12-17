2d56f2d3945de43a57f90e2016d038f4538c6dde
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:18 +0100

    2020 day 1

--- 2020/Haskell/src/Day01.hs
+++ 2020/Haskell/src/Day01.hs
@@ -1,8 +1,26 @@
 module Day01 where
 
-parseInput = id
+import Lib ( linedNums )
+import Data.Maybe ( listToMaybe )
+
+part1 :: [Int] -> Maybe Int
+part1 input = listToMaybe [x * y
+                          | x <- input
+                          , y <- input
+                          , x + y  == 2020]
+
+part2 :: [Int] -> Maybe Int
+part2 input = listToMaybe [x * y * z
+                          | x <- input
+                          , y <- input
+                          , z <- input
+                          , x + y + z == 2020]
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "../data/day01.in"
-  print input
+  input <- linedNums <$> readFile "../data/day01.in"
+  print $ part1 input
+  print $ part2 input
+
+-- 793524
+-- 61515678
