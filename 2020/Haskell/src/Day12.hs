ce33dac6a3591a3d254645fe5b7dbe36c098d490
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Datatype -> Tuple

--- 2020/Haskell/src/Day12.hs
+++ 2020/Haskell/src/Day12.hs
@@ -1,43 +1,36 @@
 module Day12 where
 
 import Advent.Coord
-    ( origin, up, down, left, right, turnLeft, turnRight )
-import Control.Lens ( Lens', (&), view, (%~), (+~), makeLenses )
-import Lib ( mannDist, Point )
-import Linear (V2 (..), (*^) )
+  ( down, left, origin, right, turnLeft, turnRight, up,)
+import Control.Lens (Lens', view, (%~), (&), (+~), _1, _2)
 import Data.List (foldl')
+import Lib (Point, mannDist)
+import Linear (V2 (..), (*^))
 
 data Command = N | S | E | W | L | R | F
     deriving (Show, Read, Eq, Ord)
 
-data Ship = Ship
-    { _dir :: !Point
-    , _pos :: !Point
-    }
-    deriving (Show, Eq, Ord)
-makeLenses ''Ship
-
 parseInput :: String -> [(Command, Int)]
 parseInput = map (\x -> (read [head x], read $ tail x)) . lines
 
-move :: Lens' Ship (V2 Int) -> Ship -> (Command, Int) -> Ship
+move :: Lens' (Point, Point) Point -> (Point, Point) -> (Command, Int) -> (Point, Point)
 move l ship c = ship & case c of
-  (N, n  ) -> l +~ n *^ up
-  (E, n  ) -> l +~ n *^ right
-  (S, n  ) -> l +~ n *^ down
-  (W, n  ) -> l +~ n *^ left
-  (_, 180) -> dir %~ negate
-  (L, 90 ) -> dir %~ turnLeft
-  (R, 90 ) -> dir %~ turnRight
-  (L, 270) -> dir %~ turnRight
-  (R, 270) -> dir %~ turnLeft
-  (F, n)   -> pos +~ (n *^ view dir ship)
+  (N, n  ) -> l  +~ n *^ up
+  (E, n  ) -> l  +~ n *^ right
+  (S, n  ) -> l  +~ n *^ down
+  (W, n  ) -> l  +~ n *^ left
+  (_, 180) -> _1 %~ negate
+  (L, 90 ) -> _1 %~ turnLeft
+  (R, 90 ) -> _1 %~ turnRight
+  (L, 270) -> _1 %~ turnRight
+  (R, 270) -> _1 %~ turnLeft
+  (F, n  ) -> _2 +~ n *^ view _1 ship
 
 part1 :: [(Command, Int)] -> Int
-part1 = mannDist (V2 0 0) . view pos . foldl' (move pos) (Ship right origin)
+part1 = mannDist (V2 0 0) . snd . foldl' (move _2) (right, origin)
 
 part2 :: [(Command, Int)] -> Int
-part2 = mannDist (V2 0 0) . view pos . foldl' (move dir) (Ship (V2 10 1) origin)
+part2 = mannDist (V2 0 0) . snd . foldl' (move _1) (V2 10 1, origin)
 
 main :: IO ()
 main = do
