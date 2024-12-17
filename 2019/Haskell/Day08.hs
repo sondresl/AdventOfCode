6462cb16bb0ec11cdd10d1f4b0e3697873963e3d
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:16 +0100

    generalised occ, moved intercalate

--- 2019/Haskell/Day08.hs
+++ 2019/Haskell/Day08.hs
@@ -8,22 +8,21 @@ color ('2':xs) (y:ys) = y : color xs ys
 color (x:xs) (_:ys) = x : color xs ys
 
 solveA :: Int -> Int -> String -> Int
-solveA w h str = (occ '1') * (occ '2')
-  where fewestZeros = minimumBy (comparing (length . filter (== '0'))) $ chunksOf (w * h) str
-        occ n = length $ filter (== n) fewestZeros
+solveA w h str = (occ '1' xs) * (occ '2' xs)
+  where occ n ls = length $ filter (== n) ls
+        xs = minimumBy (comparing (occ '0')) $ chunksOf (w * h) str
 
 solveB :: Int -> Int -> String -> String
 solveB w h str = 
   let res = foldr1 color $ chunksOf (w * h) str
-      f '1' = 'O'
+      f '1' = '#'
       f '0' = ' '
-      f a = a
-   in map f $ intercalate "\n" $ chunksOf w res
+   in intercalate "\n" . chunksOf w  $ map f res
 
 main = do
-  contents <- head . lines <$> readFile "data/input-2019-8.txt"
-  print $ solveA 25 6 contents
-  putStrLn $ solveB 25 6 contents
+  str <- head . lines <$> readFile "data/input-2019-8.txt"
+  print $ solveA 25 6 str
+  putStrLn $ solveB 25 6 str
 
 -- 1620
 -- BCYEF
