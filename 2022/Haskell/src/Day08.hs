9c4d5c8d0dceeceb1fe69129a7f21cb038c21458
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:52 +0100

    Day 8

--- 2022/Haskell/src/Day08.hs
+++ 2022/Haskell/src/Day08.hs
@@ -1,36 +1,52 @@
 module Day08 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
+import Data.List.Extra (transpose, chunksOf, nubOrdOn)
 
-part1 input = undefined
+findGrowing :: [(Int, (Int, Int))] -> [(Int, (Int, Int))]
+findGrowing xs = fromLeft xs <> fromRight xs
+  where
+    fromLeft (x:xs) = x : go x xs
+    fromLeft _ = error "from left"
+    fromRight (reverse -> (x:xs)) = x : go x xs
+    fromRight _ = error "from right"
+    go c [] = []
+    go (c, ic) ((x, ix):xs)
+      | x > c = (x, ix) : go (x, ix) xs
+      | otherwise = go (c, ic) xs
 
-part2 input = undefined
+addIx :: [[Int]] -> [(Int, (Int, Int))]
+addIx xs = do
+  (lx, ls) <- zip [1..] xs 
+  (ix, x) <- zip [1..] ls
+  pure (x, (lx, ix))
+
+fromTree :: [(Int, (Int, Int))] -> [Int]
+fromTree (map fst -> xs) = f [] xs
+  where
+    f [] (x:xs) = 0 : f [x] xs
+    f ls [x] = [0]
+    f ls (x:rs) = let len = findLen x ls * findLen x rs
+                   in len : f (x:ls) rs
+    f _ _ = error "f"
+    findLen orig [x] = 1
+    findLen orig (x:y:xs)
+      | x >= orig = 1
+      | otherwise = 1 + findLen orig (y:xs)
+    findLen _ e = error "findLen"
+
+part2 :: [[(Int, (Int, Int))]] -> Int
+part2 input = maximum $ zipWith (*) (concat hori) (concat $ transpose . transpose . transpose $ vert)
+  where
+    hori = map fromTree input
+    vert = map fromTree $ transpose input
 
 main :: IO ()
 main = do
-
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
-
-        -- print $ part1 input
-        -- print $ part2 input
+  input' <- map (map (read @Int . (:[]))). lines <$> readFile "../data/day08.in"
+  let input = chunksOf (length (head input')) $ addIx input'
+      angles xs = xs <> transpose xs
+  print . length . nubOrdOn snd . concatMap findGrowing . angles $ input
+  print $ part2 input
     
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day08.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 1647
+-- 392080
