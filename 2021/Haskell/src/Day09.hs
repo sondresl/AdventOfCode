f33ed0bd6d5c428d23295d4a8e5e23f52017e97d
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:33 +0100

    Notes on day 9

--- 2021/Haskell/src/Day09.hs
+++ 2021/Haskell/src/Day09.hs
@@ -8,19 +8,21 @@ import Control.Applicative (liftA2)
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+lowPoints :: Map (V2 Int) Int -> [(V2 Int, Int)]
+lowPoints input = Map.toList $ Map.filterWithKey lowPoint input
+  where lowPoint k v = all (v <) . mapMaybe (`Map.lookup` input) $ neighbours4 k
+
 basin :: Map (V2 Int) Int -> (V2 Int, Int) -> Int
 basin input (pos, val) = length $ bfs [pos] f
   where
-    f = filter (liftA2 (&&) (val <) (/= 9) . flip (Map.findWithDefault 0) input) . neighbours4
+    f = filter ((/= 9) . flip (Map.findWithDefault 9) input) . neighbours4
 
 main :: IO ()
 main = do
   input <- parseAsciiMap (Just . read . pure) <$> readFile "../data/day09.in"
-  let lowPoint k v = all (v <) . mapMaybe (`Map.lookup` input) $ neighbours4 k
-      lowPoints = Map.toList $ Map.filterWithKey lowPoint input
-
-  print . sum . map ((+1) . snd) $ lowPoints
-  print . product . take 3 . sortBy (flip compare) $ map (basin input) lowPoints
+  let lps = lowPoints input
+  print . sum . map ((+1) . snd) $ lps
+  print . product . take 3 . sortBy (flip compare) $ map (basin input) lps
 
 -- 545
 -- 950600
