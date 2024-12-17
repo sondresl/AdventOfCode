7fa4d108c68388faa43b929fe1b6119e795c2cf7
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:18 +0100

    Solution day 2

--- 2020/Haskell/src/Day02.hs
+++ 2020/Haskell/src/Day02.hs
@@ -1,8 +1,46 @@
 module Day02 where
 
-parseInput = id
+import Lib ( count )
+import Control.Lens
+    ( (^?), Ixed(ix) )
+import Text.ParserCombinators.Parsec
+    ( alphaNum, char, digit, letter, space, string, many1, parse )
+
+data Pass = P { _a :: Int
+              , _b :: Int
+              , _ch :: Char
+              , _password :: String
+              }
+  deriving (Show, Eq, Ord)
+
+parseInput :: String -> [Pass]
+parseInput = either (error . show) id . traverse (parse pass "") . lines
+ where
+  num = read <$> many1 digit
+  pass =
+    P <$> num <* char '-'
+      <*> num <* space
+      <*> letter <* string ": "
+      <*> many1 alphaNum
+
+part1 :: [Pass] -> Int
+part1 = count valid
+  where
+   valid (P a b l str) = a <= le && le <= b
+    where le = count (== l) str
+
+part2 :: [Pass] -> Int
+part2 = count pos
+  where
+    pos (P a b l str) = aa && not bb || not aa && bb
+      where aa = str ^? ix (a - 1) == Just l
+            bb = str ^? ix (b - 1) == Just l
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "../data/day02.in"
-  print input
+  print $ part1 input
+  print $ part2 input
+
+-- 517
+-- 284
