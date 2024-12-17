ed7d150c9c979dbc2583fbcdcf2e8ffc04aa35d2
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:53 +0100

    Day 11 better

--- 2023/Haskell/src/Day11.hs
+++ 2023/Haskell/src/Day11.hs
@@ -1,41 +1,27 @@
 module Day11 where
 
-import Lib (mannDist, Point, parseAsciiMap, combinations)
+import Lib (tuple, mannDist, Point, parseAsciiMap, combinations)
 import Control.Monad (guard)
-import Data.List.Extra (transpose)
+import Data.List.Extra (transpose, mapAccumL)
 import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
 import Linear (V2(..))
 
-findSum :: String -> Int -> Int
-findSum input addRows = sum $ do
-  let expanded = Set.toList $ expand input addRows 
-  [x, y] <- combinations 2 expanded
-  pure $ mannDist x y
-
-expand :: String -> Int -> Set Point
-expand input addRows = Set.map (\(V2 x y) -> V2 (cs x) (rs y)) coords
+expand :: String -> Int -> [Point]
+expand input addRows = map (\(V2 x y) -> V2 (cs x) (rs y)) $ parseInput input
   where
-    coords = parseInput input
     rs y = let Just y' = lookup y (rows addRows $ lines input) in y + y'
     cs x = let Just x' = lookup x (rows addRows $ transpose $ lines input) in x + x'
-
-rows :: Int -> [String] -> [(Int, Int)]
-rows increase = zip [0..] . go 0
-  where
-    go n [] = []
-    go n (x:xs) = if all (== '.') x then n : go (n + increase) xs else n : go n xs
+    rows inc = zip [0..] . snd . mapAccumL f 0
+      where f st row = (if all (== '.') row then st + inc else st, st)
+    parseInput = Map.keys . parseAsciiMap f
+      where f x = guard (x == '#') *> Just ()
 
 main :: IO ()
 main = do
   input <- readFile "../data/day11.in"
-  print $ findSum input 1
-  print $ findSum input 999999
-
-parseInput :: String -> Set Point
-parseInput = Map.keysSet . parseAsciiMap f
-  where f x = guard (x == '#') *> Just ()
+  let findSum = sum . map (uncurry mannDist . tuple) . combinations 2
+  print . findSum $ expand input 1
+  print . findSum $ expand input 999999
 
 -- 10154062
 -- 553083047914
