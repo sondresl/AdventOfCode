6f5999d664d8c8b24c1c6cead13422eaf0625994
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:17 +0100

    Minor changes

--- 2017/07.py
+++ 2017/07.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
+from collections import Counter
 import re
-
-class FoundDiff(Exception):
-    pass
 
 def read_file(filename):
     nodes = {}
@@ -12,9 +9,9 @@ def read_file(filename):
             names = re.findall(r'[a-zA-Z]+', line)
             name = names[0]
             if len(names) > 1:
-                nodes[name] = (weight, tuple(names[1:]))
+                nodes[name] = (weight, names[1:])
             else:
-                nodes[name] = (weight, tuple())
+                nodes[name] = (weight, [])
     return nodes
 
 def find_root(nodes):
@@ -27,25 +24,31 @@ def find_root(nodes):
         else:
             return root
 
-def find_wrong_weight(nodes):
-    root = find_root(nodes)
-    ws = [find_weight(c, nodes) for c in nodes[root][1]]
-    print(ws)
-    return max(ws) - min(ws)
+def find_wrong_weight(n, nodes):
+    ws = []
+    for sub in nodes[n][1]:
+        (t, val) = find_wrong_weight(sub, nodes)
+        if t:
+            return (t, val)
+        ws.append(val)
+    if len(set(ws)) <= 1:
+        return (False, sum(ws) + nodes[n][0])
+    else:
+        count = Counter(ws).most_common()
+        tar = nodes[n][1][ws.index(count[-1][0])]
+        tar_sum = nodes[tar][0]
+        diff = count[0][0] - count[1][0]
+        return (True, tar_sum + diff)
 
 def find_weight(n, nodes):
     ws = [find_weight(c, nodes) for c in nodes[n][1]]
     return sum(ws) + nodes[n][0]
 
 def main():
-    nodes = read_file('input/07.in')
-    print('Part 1:', find_root(nodes))
-    print('Part 2:', find_wrong_weight(nodes))
-
+    nodes = read_file('data/07.in')
+    root = find_root(nodes)
+    print('Part 1:', root)
+    print('Part 2:', find_wrong_weight(root, nodes)[1])
 
 if __name__ == "__main__":
     main()
-
-# 9
-# 42
-# 6959
