b35d76557f916ae5c4ea76b62d16b2993b9d7322
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:52 +0100

    Day 12

--- 2022/Haskell/src/Day12.hs
+++ 2022/Haskell/src/Day12.hs
@@ -1,36 +1,30 @@
 module Day12 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
+import Lib (parseAsciiMap, bfs, neighbours4)
+import Linear (V2(..))
+import Data.List.Extra (find)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
 
-part1 input = undefined
+type GPS = Map (V2 Int) Char
+type Pos = (V2 Int, Int) -- Position, length to that position
 
-part2 input = undefined
+move :: GPS -> (GPS -> [Pos]) -> [Pos]
+move input start = bfs (start input) (ns input) fst
+  where 
+    ns mp (pos, dist) = map (,dist + 1) . filter (\p -> p `Map.member` mp && (convert (mp Map.! p) <= succ next)) $ neighbours4 pos
+      where next = convert $ mp Map.! pos
+            convert 'S' = 'a'
+            convert 'E' = 'z'
+            convert c = c
 
 main :: IO ()
 main = do
+  input <- parseAsciiMap Just <$> readFile "../data/day12.in"
+  let start str = map (,0) . Map.keys . Map.filter (`elem` str)
+      run = fmap snd . find ((=='E') . (input Map.!) . fst) . move input
+  print $ run (start "S")
+  print $ run (start "Sa")
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
-
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day12.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 462
+-- 451
