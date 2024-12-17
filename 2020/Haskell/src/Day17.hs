7123cb64ced179d8a2db3b08dca785bc06742891
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Single function for neighbours

--- 2020/Haskell/src/Day17.hs
+++ 2020/Haskell/src/Day17.hs
@@ -1,11 +1,11 @@
 module Day17 where
 
-import Lib ( count, parseAsciiMap )
-import Data.Maybe ( mapMaybe )
-import Linear ( V2(..), V3(..), V4(..) )
 import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
 import qualified Data.Set as Set
-import           Data.Set   ( Set )
+import Lib (count, parseAsciiMap)
+import Linear (V2 (..), V3 (..), V4 (..))
 
 parseInput :: String -> Set (V2 Int)
 parseInput = Map.keysSet . parseAsciiMap f
@@ -18,28 +18,33 @@ step genNearby input = Set.fromList . mapMaybe f . Set.toList $ candidates
   where
     candidates = Set.fromList (concatMap genNearby input) `Set.union` input
     f p = case (p `Set.member` input, Lib.count (`Set.member` input) (genNearby p)) of
-            (True, 2) -> Just p
-            (True, 3) -> Just p
-            (False, 3) -> Just p
-            _ -> Nothing
+        (True, 2) -> Just p
+        (True, 3) -> Just p
+        (False, 3) -> Just p
+        _ -> Nothing
+
+-- Will generate neighbours for V2, V3, V4 +++
+neighs ::
+    (Traversable t, Applicative t, Num a, Eq (t a)) =>
+    t a ->
+    [t a]
+neighs p = do
+    n <- tail $ sequenceA (pure [0, 1, -1])
+    pure $ (+) <$> p <*> n
 
 part1 :: Set (V3 Int) -> Int
-part1 = Set.size . (!! 6) . iterate (step allNei)
-  where
-    allNei p = tail $ (p +) <$> (V3 <$> [0,1,-1] <*> [0,1,-1] <*> [0,1,-1])
+part1 = Set.size . (!! 6) . iterate (step neighs)
 
 part2 :: Set (V4 Int) -> Int
-part2 = Set.size . (!! 6) . iterate (step allNei)
-  where
-    allNei p = tail $ (p +) <$> (V4 <$> [0,1,-1] <*> [0,1,-1] <*> [0,1,-1] <*> [0,1,-1])
+part2 = Set.size . (!! 6) . iterate (step neighs)
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "../data/day17.in"
-  let v2tov3 (V2 x y) = V3 x y 0
-      v2tov4 (V2 x y) = V4 x y 0 0
-  print $ part1 (Set.map v2tov3 input)
-  print $ part2 (Set.map v2tov4 input)
+    input <- parseInput <$> readFile "../data/day17.in"
+    let v2tov3 (V2 x y) = V3 x y 0
+        v2tov4 (V2 x y) = V4 x y 0 0
+    print $ part1 (Set.map v2tov3 input)
+    print $ part2 (Set.map v2tov4 input)
 
 -- 240
 -- 1180
