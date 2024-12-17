a42e05b27955f9586b5631f3a1810f78cab819a7
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Make separate score function

--- 2020/Haskell/src/Day22.hs
+++ 2020/Haskell/src/Day22.hs
@@ -6,29 +6,32 @@ import Data.Sequence (Seq (..), (|>))
 import qualified Data.Sequence as Seq
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Lib (tuple)
 import Data.Tuple.Extra (both)
+import Lib (tuple)
+
+score :: Foldable t => t Int -> Int
+score = sum . zipWith (*) [1 ..] . reverse . toList
 
 play :: Seq Int -> Seq Int -> Int
-play as Empty = sum . zipWith (*) [1 ..] . reverse $ toList as
-play Empty bs = sum . zipWith (*) [1 ..] . reverse $ toList bs
+play as Empty = score as
+play Empty bs = score bs
 play (a :<| xs) (b :<| bs)
     | a > b = play (xs |> a |> b) bs
     | b > a = play xs (bs |> b |> a)
 
 play' :: Set (Seq Int, Seq Int) -> Seq Int -> Seq Int -> Either Int Int
-play' _ as Empty = Left . sum . zipWith (*) [1 ..] . reverse $ toList as
-play' _ Empty bs = Right . sum . zipWith (*) [1 ..] . reverse $ toList bs
+play' _ as Empty = Left $ score as
+play' _ Empty bs = Right $ score bs
 play' seen x@(a :<| as) y@(b :<| bs)
-    | Set.member (x, y) seen = Left . sum . zipWith (*) [1 ..] . reverse $ toList x
+    | Set.member (x, y) seen = Left $ score x
     | a > Seq.length as || b > Seq.length bs =
         case compare a b of
-            LT -> play' (Set.insert (x, y) seen) as (bs |> b |> a)
+            LT -> play' (Set.insert (x, y) seen) as             (bs |> b |> a)
             GT -> play' (Set.insert (x, y) seen) (as |> a |> b) bs
     | otherwise =
         case play' Set.empty (Seq.take a as) (Seq.take b bs) of
-            Left _ -> play' (Set.insert (x, y) seen) (as |> a |> b) bs
-            Right _ -> play' (Set.insert (x, y) seen) as (bs |> b |> a)
+            Left  _ -> play' (Set.insert (x, y) seen) (as |> a |> b) bs
+            Right _ -> play' (Set.insert (x, y) seen) as             (bs |> b |> a)
 
 main :: IO ()
 main = do
