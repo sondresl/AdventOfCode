fa00afef46f6e5b94b7970d974d3974ec3926bea
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:54 +0100

    Remove imports

--- 2024/Haskell/src/Day11.hs
+++ 2024/Haskell/src/Day11.hs
@@ -1,14 +1,13 @@
 module Day11 where
 
 import Lib (iterateN, allNums)
-import Data.Digits (digits, unDigits)
 import Data.IntMap (IntMap)
-import qualified Data.IntMap as IntMap
+import qualified Data.IntMap as IM
 
 blinkStones :: IntMap Int -> IntMap Int
-blinkStones stones = IntMap.fromListWith (+) $ do
-  (stone, count) <- IntMap.assocs stones
-  map (,count) $ blink stone
+blinkStones stones = IM.fromListWith (+) $ do
+  (stone, count) <- IM.assocs stones
+  (,count) <$> blink stone
 
 blink :: Int -> [Int]
 blink 0 = [1]
@@ -17,12 +16,11 @@ blink n
   | otherwise = [left, right]
     where
       digs = floor (logBase 10 (fromIntegral n)) + 1
-      left  = n `div` 10^(digs `div` 2)
-      right = n `mod` 10^(digs `div` 2)
+      (left, right)  = n `divMod` (10^(digs `div` 2))
 
 main :: IO ()
 main = do
-  input <- IntMap.fromList . map (,1) . allNums <$> readFile "../data/day11.in"
+  input <- IM.fromListWith (+) . map (,1) . allNums <$> readFile "../data/day11.in"
   print $ sum $ iterateN 25 blinkStones input
   print $ sum $ iterateN 75 blinkStones input
 
