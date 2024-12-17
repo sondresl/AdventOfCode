dd35c967534831618cf6c65c577953bfd9fd5e00
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:33 +0100

    Day 4 2021

--- 2021/Haskell/src/Day04.hs
+++ 2021/Haskell/src/Day04.hs
@@ -1,29 +1,39 @@
 module Day04 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Data.List.Extra
-import Text.ParserCombinators.Parsec hiding (count)
+import Lib (takeUntil, commaNums)
+import Data.List.Extra (transpose, splitOn, maximumOn, minimumOn, sortOn)
+import Data.Tuple.Extra ((&&&), second, both)
 
-part1 input = undefined
+type Board = [[Int]]
+type Result = (Int, Int)
 
-part2 input = undefined
+playAll :: [Int] -> [Board] -> Result
+playAll xs = both (uncurry (*)) 
+           . (head &&& last) 
+           . map (second (xs !!)) 
+           . sortOn snd 
+           . map ((play xs) . ((<>) <*> transpose))
+
+play :: [Int] -> Board -> (Int, Int) -- Score, turns
+play nums board = (score . last &&& subtract 1 . length)
+                . tail 
+                . takeUntil (any null) 
+                $ scanl (flip $ map . filter . (/=)) board nums
+  where 
+    score = sum . map sum . take 5
 
 main :: IO ()
 main = do
+  (nums, boards) <- parseInput <$> readFile "../data/day04.in"
+  let (part1, part2) = playAll nums boards 
+  print part1
+  print part2
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
+parseInput :: String -> ([Int], [Board])
+parseInput input = (nums, grids)
+  where (x:xs) = splitOn "\n\n" input
+        nums = commaNums x
+        grids = map ((map (map read . words)) . lines) xs
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day04.in"
-
-parseInput = id
+-- 38594
+-- 21184
