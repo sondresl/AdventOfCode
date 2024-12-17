8f9b13b6db72f0889b47a415722e203cf9fc73b3
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:52 +0100

    Day 5 initial

--- 2023/Haskell/src/Day05.hs
+++ 2023/Haskell/src/Day05.hs
@@ -1,41 +1,52 @@
 module Day05 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.RawString.QQ
-import Text.ParserCombinators.Parsec hiding (count)
+import Lib (allNums)
+import Data.List.Extra ( splitOn, find, chunksOf )
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 
-part1 input = undefined
+findLocation :: [[[Integer]]] -> Integer -> Integer
+findLocation allMaps initialSeed = foldl go initialSeed allMaps
+  where
+    go seed maps = case find inRange maps of
+                     Nothing -> seed
+                     Just [dest, src, range] -> dest + (seed - src)
+      where
+        inRange [dest, src, range] = src <= seed && seed <= src + range - 1
 
-part2 input = undefined
+isSeed :: [Integer] -> Integer -> Bool
+isSeed (chunksOf 2 -> pairs) cand = 
+  any (\[start, range] -> start <= cand && cand <= start + range - 1) pairs
 
+findSeed :: [[[Integer]]] -> Integer -> Integer
+findSeed allMaps initialLoc = foldl go initialLoc allMaps
+  where
+    go loc maps = case find inRange maps of
+                     Nothing -> loc
+                     Just [dest, src, range] -> src + (loc - dest)
+      where
+        inRange [dest, src, range] = dest <= loc && loc <= dest + range - 1
+        
 main :: IO ()
 main = do
+  input <- parseInput <$> readFile "../data/day05.in"
+  let (seeds, maps) = input
+  print $ minimum $ map (findLocation maps) seeds
+  let revMaps = reverse maps
+  -- Original brute force search, set the start number gradually higher to control brute force
+  -- let start = 20000000
+  -- print $ find (isSeed seeds . snd) $ zip [start..] $ map (findSeed revMaps) [start..start + 10000000]
+  print $ isSeed seeds $ findSeed revMaps 24261546
 
-  let run str input = do
-        putStrLn str
-        print input
+parseInput :: String -> ([Integer], [[[Integer]]])
+parseInput input = (seeds, maps)
+  where
+  seeds :: [Integer]
+  seeds = allNums ss
+  (ss:rest) = splitOn "\n\n" input
+  maps = map (mkRange . lines) rest
+  mkRange (_:xs) = map (map read . words) xs
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" testInput
+-- 261668924
+-- 24261546
 
-  -- input <- parseInput <$> readFile "../data/day05.in"
-  -- run "\nActual:\n\n" input
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
-
-testInput = [r|
-|]
