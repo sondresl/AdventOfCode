5793af1c3cd6f36fe82e4fbd33307c30ecd5b194
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:19 +0100

    Some optimizations to day 11

--- 2020/Haskell/src/Day11.hs
+++ 2020/Haskell/src/Day11.hs
@@ -1,54 +1,44 @@
 module Day11 where
 
-import Lib
-import Data.Maybe
+import Lib ( count, Point, parseAsciiMap, neighbours, fixedPoint )
+import Data.Maybe ( mapMaybe )
 import qualified Data.Map as Map
 import           Data.Map   ( Map )
 import Linear ( V2(..) )
+import Data.Foldable (find)
 
-data Seat = Free | Taken
-  deriving (Show, Eq, Ord)
-
-parseInput :: String -> Map Point Seat
+parseInput :: String -> Map Point Bool
 parseInput = parseAsciiMap f
   where
-    f 'L' = Just Free
+    f 'L' = Just True
     f _ = Nothing
 
-seatsSeen :: Map Point Seat -> Point -> [Seat]
-seatsSeen input seat = mapMaybe seats angles
+step :: (Point -> [Point]) -> Int -> Map Point Bool -> Map Point Bool
+step seats cmp input = Map.mapWithKey check input
+  where check k v =
+          let nbs = count id $ mapMaybe (`Map.lookup` input) $ seats k
+           in case (v, nbs) of
+                (False, 0) -> True
+                (True, n) | n >= cmp -> False
+                (n, _) -> n
+
+-- Generate the seats seen by each seat when looking in all eight directions.
+diagonalSeats :: Map Point Bool -> Map Point [Point]
+diagonalSeats input = Map.mapWithKey (\k _ -> mapMaybe (seats k) angles) input
   where
-    seats f = listToMaybe . mapMaybe (`Map.lookup` input) . takeWhile inBounds . tail $ iterate f seat
+    seats seat f = find (`Map.member` input) . takeWhile inBounds . tail $ iterate f seat
     angles = fmap (+) . tail $ V2 <$> [0, 1, -1] <*> [0,1,-1]
     inBounds (V2 x y) = x < 96 && y < 90 && x >= 0 && y >= 0
 
-step :: Map Point Seat -> Map Point Seat
-step input = Map.mapWithKey check input
-  where check k v =
-          let nbs = count (== Taken) . mapMaybe (`Map.lookup` input) $ neighbours k
-            in case (v, nbs) of
-                (Free, 0) -> Taken
-                (Taken, n) | n >= 4 -> Free
-                (n, _) -> n
-
-step' :: Map Point Seat -> Map Point Seat
-step' input = Map.mapWithKey check input
-  where check k v =
-          let nbs = count (== Taken) $ seatsSeen input k
-           in case (v, nbs) of
-                (Free, 0) -> Taken
-                (Taken, n) | n >= 5 -> Free
-                (n, _) -> n
-
-run :: (Map Point Seat -> Map Point Seat) -> Map Point Seat -> Maybe Int
-run f = fmap (count (==Taken)) . firstRepeat . iterate f
+run :: (Map Point Bool -> Map Point Bool) -> Map Point Bool -> Maybe Int
+run f = fmap (count id) . fixedPoint f
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "../data/day11.in"
-  print $ run step input
-  print $ run step' input
-
+  print $ run (step neighbours 4) input
+  let p2seats = diagonalSeats input
+  print $ run (step (p2seats Map.!) 5) input
 
 -- 2243
 -- 2027
