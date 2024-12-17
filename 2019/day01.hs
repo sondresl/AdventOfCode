52242fd30a2d9f06cbe43097df16b6a8526ef748
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:16 +0100

    Name change

--- 2019/day01.hs
+++ 2019/day01.hs
@@ -4,15 +4,17 @@
 strToInts :: String -> [Int]
 strToInts = map read . lines
 
+calcFuel :: Int -> Int
+calcFuel = subtract 2 . flip div 3
+
 solve :: [Int] -> Int
 solve = sum . map calcFuel
-  where calcFuel = subtract 2 . flip div 3
 
 solve2 :: [Int] -> Int
 solve2 = sum . map calcFuelFuel
-  where calcFuel = subtract 2 . flip div 3
-        calcFuelFuel = sum . takeWhile (> 0) . iterate calcFuel . calcFuel
+  where calcFuelFuel = sum . takeWhile (> 0) . iterate calcFuel . calcFuel
 
+main :: IO ()
 main = do
   contents <- strToInts <$> readFile "data/input-2019-1.txt"
   print $ solve contents
