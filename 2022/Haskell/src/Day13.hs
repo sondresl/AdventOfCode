d63af5b6467eb8bcbf1fa3e08740effc6098a4c2
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:52 +0100

    Day 13

--- 2022/Haskell/src/Day13.hs
+++ 2022/Haskell/src/Day13.hs
@@ -1,36 +1,40 @@
 module Day13 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
+import Lib (tuple)
+import Data.List.Extra (splitOn, elemIndex, sort)
+import Text.ParserCombinators.Parsec (between, char, sepBy, many1, digit, parse, (<|>))
 
-part1 input = undefined
+newtype Tree = Tree [Either Int Tree]
+  deriving (Show, Eq)
 
-part2 input = undefined
+instance Ord Tree where
+  compare = solve
+
+solve :: Tree -> Tree -> Ordering
+solve (Tree ls) (Tree rs) = f ls rs
+  where
+    f [] (_:_) = LT
+    f _ [] = GT
+    f (Left l:ls)  (Left r:rs)  = l `compare` r <> f ls rs
+    f (Right l:ls) (Right r:rs) = solve l r <> f ls rs
+    f (Left l:ls)  (Right (Tree r):rs) = f [Left l] r <> f ls rs
+    f (Right (Tree l):ls) (Left r:rs)  = f l [Left r] <> f ls rs
+
+part2trees :: [Tree]
+part2trees = [Tree [Right (Tree [Left 2])], Tree [Right (Tree [Left 6])]]
 
 main :: IO ()
 main = do
+  input <- parseInput <$> readFile "../data/day13.in"
+  print $ sum . map snd $ filter ((==LT) . fst) . flip zip [1..] $ map (uncurry compare) input
+  let part2 = sort $ concatMap (\(x,y) -> [x,y]) input <> part2trees
+  print $ product . map (+1) <$> traverse (`elemIndex` part2) part2trees
 
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
+parseInput :: String -> [(Tree, Tree)]
+parseInput = map (tuple . either (error . show) id . traverse (parse p "") . lines) . splitOn "\n\n"
+  where
+    p = Tree <$> between (char '[') (char ']') f
+    f = sepBy ((Right <$> p) <|> (Left . read <$> many1 digit)) (char ',')
 
-        -- print $ part1 input
-        -- print $ part2 input
-    
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day13.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+-- 5659
+-- 22110
