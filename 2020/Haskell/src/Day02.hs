37f0bf00bc975132c005de63060f2bcefd332bad
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:18 +0100

    /= is xor for boolean

--- 2020/Haskell/src/Day02.hs
+++ 2020/Haskell/src/Day02.hs
@@ -1,40 +1,35 @@
 module Day02 where
 
-import Lib ( count )
-import Control.Lens
-    ( (^?), Ixed(ix) )
-import Text.ParserCombinators.Parsec
-    ( alphaNum, char, digit, letter, space, string, many1, parse )
+import Lib (count)
+import Control.Lens (Ixed (ix), (^?))
+import Text.ParserCombinators.Parsec (alphaNum, char, digit, letter, many1, parse, space, string)
 
-data Pass = P { _a :: Int
-              , _b :: Int
-              , _ch :: Char
-              , _password :: String
-              }
-  deriving (Show, Eq, Ord)
+data Password = P Int Int Char String
 
-parseInput :: String -> [Pass]
-parseInput = either (error . show) id . traverse (parse pass "") . lines
- where
-  num = read <$> many1 digit
-  pass =
-    P <$> num <* char '-'
-      <*> num <* space
-      <*> letter <* string ": "
-      <*> many1 alphaNum
+parseInput :: String -> [Password]
+parseInput = either (error . show) id . traverse (parse parsePassword "") . lines
+  where
+    num = read <$> many1 digit
+    parsePassword =
+      P <$> num <* char '-'
+        <*> num <* space
+        <*> letter <* string ": "
+        <*> many1 alphaNum
 
-part1 :: [Pass] -> Int
+part1 :: [Password] -> Int
 part1 = count valid
   where
-   valid (P a b l str) = a <= le && le <= b
-    where le = count (== l) str
+    valid (P a b l str) = a <= le && le <= b
+      where
+        le = count (== l) str
 
-part2 :: [Pass] -> Int
-part2 = count pos
+part2 :: [Password] -> Int
+part2 = count valid
   where
-    pos (P a b l str) = aa && not bb || not aa && bb
-      where aa = str ^? ix (a - 1) == Just l
-            bb = str ^? ix (b - 1) == Just l
+    valid (P a b l str) = aa /= bb
+      where
+        aa = str ^? ix (a - 1) == Just l
+        bb = str ^? ix (b - 1) == Just l
 
 main :: IO ()
 main = do
