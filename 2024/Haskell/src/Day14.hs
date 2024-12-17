43a4e43111c43d61fcac39e07cd7f8eae5f6844c
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:54 +0100

    clean up day 14

--- 2024/Haskell/src/Day14.hs
+++ 2024/Haskell/src/Day14.hs
@@ -1,7 +1,7 @@
 module Day14 where
 
 import Lib
-import Advent.Coord
+import Advent.Search
 import Data.Maybe
 import Control.Lens
 import Control.Monad
@@ -17,84 +17,79 @@ import Linear hiding (trace)
 
 type Robot = (Point, Point)
 
-part1 input = product . freqs $ filter (/= 0) bots
+bounds :: Point
+bounds = V2 101 103
+
+part1 :: [Robot] -> Int
+part1 = product . freqs . mapMaybe (score . fst . iterateN 100 move)
   where
-    bots = map (score bounds . iterateN 100 (move bounds)) input
-    bounds = V2 101 103
+    V2 bx by = bounds
+    score (V2 px py)
+      | px < bx `div` 2 && py < by `div` 2 = Just 1
+      | px > bx `div` 2 && py < by `div` 2 = Just 2
+      | px > bx `div` 2 && py > by `div` 2 = Just 3
+      | px < bx `div` 2 && py > by `div` 2 = Just 4
+      | otherwise = Nothing
 
-part2 :: [Robot] -> Maybe (Int, [Robot])
-part2 input = find tree . zip [0..] $ iterate (map (move bounds)) input
+part2 :: [Robot] -> Maybe Int
+part2 input = fst <$> (find tree . zip [0..] $ iterate (map move) input)
   where 
-    bounds = V2 101 103
-    tree (ix, bots) = all hasNeighbour botSet
+    tree (ix, bots) = (> 50) . maximum $ map Set.size $ regions botSet
       where 
         botSet = Set.fromList (map fst bots)
+        regions = floodFill (filter (`Set.member` botSet) . ordinalNeighbours)
         hasNeighbour b = any (`Set.member` botSet) $ neighbours b
 
-part3 :: [Robot] -> [[Point]]
-part3 input = map (map fst) $ iterate (map (move bounds)) input
-  where 
-    bounds = V2 101 103
-    tree (ix, bots) = all hasNeighbour botSet
-      where 
-        botSet = Set.fromList (map fst bots)
-        hasNeighbour b = any (`Set.member` botSet) $ neighbours b
-
-move :: Point -> Robot -> Robot
-move bounds (loc, dir) = (loc', dir)
+move :: Robot -> Robot
+move (loc, dir) = (loc', dir)
   where loc' = liftA2 mod (loc + dir) bounds
-
-score :: Point -> Robot -> Int
-score bounds@(V2 bx by) (V2 px py, _)
-  | px == bx `div` 2 || py == by `div` 2 = 0
-  | px < bx `div` 2 && py < by `div` 2   = 1
-  | px > bx `div` 2 && py < by `div` 2   = 2
-  | px > bx `div` 2 && py > by `div` 2   = 3
-  | px < bx `div` 2 && py > by `div` 2   = 4
 
 main :: IO ()
 main = do
-
-  let run str input = do
-        putStrLn str
-        let r = (V2 2 4, V2 2 (-3))
-            -- bounds = V2 101 103
-            bounds = V2 11 7
-        print $ part1 input
-        -- print $ fst <$> part2 input
-        -- mapM_ (putStrLn . display) $ part3 input
-        let result = drop 939 $ part3 input
-        let button n xs = do
-             _ <- getLine
-             putStrLn . display $ head xs
-             print n
-             button (n + 103) (drop 103 xs)
-        button 939 result
-
-    
-
   input <- parseInput <$> readFile "../data/day14.in"
-  run "\nActual:\n\n" input
+  print $ part1 input
+  print $ part2 input
 
-parseInput :: String -> [(Point, Point)]
+  -- Show the tree:
+  -- putStrLn . display . map (over _y (`subtract` 103) . fst) $ iterateN 6398 (map (move bounds)) input
+
+parseInput :: String -> [Robot]
 parseInput = map (f . allNums) . lines
   where f [px, py, vx, vy] = (V2 px py, V2 vx vy)
 
 -- 219512160
--- not 45
-
-testInput = [r|p=0,4 v=3,-3
-p=6,3 v=-1,-3
-p=10,3 v=-1,2
-p=2,0 v=2,-1
-p=0,0 v=1,3
-p=3,0 v=-2,-2
-p=7,6 v=-1,-3
-p=3,0 v=-1,-2
-p=9,3 v=2,3
-p=7,3 v=-1,2
-p=2,4 v=2,-3
-p=9,5 v=-3,-3
-|]
-
+-- 6398
+-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+-- x                             x
+-- x                             x
+-- x                             x
+-- x                             x
+-- x              x              x
+-- x             xxx             x
+-- x            xxxxx            x
+-- x           xxxxxxx           x
+-- x          xxxxxxxxx          x
+-- x            xxxxx            x
+-- x           xxxxxxx           x
+-- x          xxxxxxxxx          x
+-- x         xxxxxxxxxxx         x
+-- x        xxxxxxxxxxxxx        x
+-- x          xxxxxxxxx          x
+-- x         xxxxxxxxxxx         x
+-- x        xxxxxxxxxxxxx        x
+-- x       xxxxxxxxxxxxxxx       x
+-- x      xxxxxxxxxxxxxxxxx      x
+-- x        xxxxxxxxxxxxx        x
+-- x       xxxxxxxxxxxxxxx       x
+-- x      xxxxxxxxxxxxxxxxx      x
+-- x     xxxxxxxxxxxxxxxxxxx     x
+-- x    xxxxxxxxxxxxxxxxxxxxx    x
+-- x             xxx             x
+-- x             xxx             x
+-- x             xxx             x
+-- x                             x
+-- x                             x
+-- x                             x
+-- x                             x
+-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
