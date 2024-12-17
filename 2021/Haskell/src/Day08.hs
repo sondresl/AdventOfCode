0a2b5d4131477d264ddff225eb722761b9314a2f
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:02:33 +0100

    Day 8 loeb

--- 2021/Haskell/src/Day08.hs
+++ 2021/Haskell/src/Day08.hs
@@ -1,36 +1,42 @@
 module Day08 where
 
-import Lib
-import Advent.Coord
-import Data.Maybe
-import Control.Lens
-import Control.Monad
-import Control.Monad.State
-import Data.List.Extra
+import Lib (count)
+import Data.List.Extra (sort, delete, (\\))
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.ParserCombinators.Parsec hiding (count)
+import Data.Digits (unDigits)
 
-part1 input = undefined
+loeb :: Functor f => f (f a -> a) -> f a
+loeb x = go where go = fmap ($ go) x
 
-part2 input = undefined
+determine :: [String] -> Map String Int
+determine xs = Map.fromList $ flip zip [0..] $
+  loeb [ \x -> head $ delete (x !! 6) $ delete (x !! 9) $ filter ((== 6) . length) xs -- zero
+       , const $ head $ filter ((== 2) . length) xs -- one
+       , \x -> head $ delete (x !! 5) $ delete (x !! 3) $ filter ((== 5) . length) xs -- two
+       , \x -> head $ filter (\fives -> all (`elem` fives) (x !! 1)) $ filter ((== 5) . length) xs -- three
+       , const $ head $ filter ((== 4) . length) xs -- four
+       , \x -> head $ filter (\threes -> all (`elem` threes) ((x !! 4) \\ (x !! 1))) $ filter ((== 5) . length) xs -- five
+       , \x -> head $ filter (\sixes -> not $ all (`elem` sixes) (x !! 1)) $ filter ((== 6) . length) xs -- six
+       , const $ head $ filter ((== 3) . length) xs -- seven
+       , const $ head $ filter ((== 7) . length) xs -- eight
+       , \x -> head $ filter (\sixes -> all (`elem` sixes) (x !! 4)) $ filter ((== 6) . length) xs -- nine
+       ]
+
+solve :: ([Int] -> Int) -> [([String], [String])] -> Int
+solve f = sum . map run
+  where
+    run (signals, outputs) = f $ map (res Map.!) outputs
+      where
+        res = determine signals
 
 main :: IO ()
 main = do
-
-  let run str file = do
-        input <- parseInput <$> readFile file
-        putStrLn str
-        print input
-
-        -- print $ part1 input
-        -- print $ part2 input
+  input <- parseInput <$> readFile "../data/day08.in"
+  print $ solve (count (`elem` [1,4,7,8])) input
+  print $ solve (unDigits 10) input
     
-  run "\nTest:\n\n" "../data/test.in"
-  -- run "\nActual:\n\n" "../data/day08.in"
-
-parseInput = id
-
--- parseInput = either (error . show) id . traverse (parse p "") . lines
---   where
---     p = undefined
+parseInput :: String -> [([String], [String])]
+parseInput = map (f . words) . lines
+  where
+    f input = (map sort $ take 10 input, map sort $ drop 11 input)
