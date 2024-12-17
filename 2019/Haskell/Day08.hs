fa039e695e951084fe1f308ee54b52546980cce1
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-17 17:01:16 +0100

    Clean up, used chunksOf

--- 2019/Haskell/Day08.hs
+++ 2019/Haskell/Day08.hs
@@ -1,36 +1,24 @@
-import Data.List
 import Data.Ord
-import Data.Function
-import Debug.Trace
-
-layer :: Int -> Int -> String -> [String]
-layer _ _ [] = []
-layer w h str = take (w * h) str : layer w h (drop (w * h) str)
+import Data.List
+import Data.List.Split (chunksOf)
 
 color :: String -> String -> String
 color "" "" = ""
 color ('2':xs) (y:ys) = y : color xs ys
 color (x:xs) (_:ys) = x : color xs ys
 
-image :: Int -> String -> String
-image w "" = "\n"
-image w str = (take w str) ++ "\n" ++ image w (drop w str)
-
 solveA :: Int -> Int -> String -> Int
-solveA w h str = ones * twos
-  where new = layer w h str
-        fewestZeros = minimumBy (comparing (length . filter (== '0'))) new
-        ones = length $ filter (== '1') fewestZeros
-        twos = length $ filter (== '2') fewestZeros
+solveA w h str = (occ '1') * (occ '2')
+  where fewestZeros = minimumBy (comparing (length . filter (== '0'))) $ chunksOf (w * h) str
+        occ n = length $ filter (== n) fewestZeros
 
 solveB :: Int -> Int -> String -> String
 solveB w h str = 
-  let ls = layer w h str
-      res = foldr1 color ls
+  let res = foldr1 color $ chunksOf (w * h) str
       f '1' = 'O'
       f '0' = ' '
       f a = a
-   in map f $ image w res
+   in map f $ intercalate "\n" $ chunksOf w res
 
 main = do
   contents <- head . lines <$> readFile "data/input-2019-8.txt"
