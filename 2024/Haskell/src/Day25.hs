9b562153445f4eba523fb2ac713fd7f20c2c5fbc
Sondre Skaflem Lunde <sondreslunde@gmail.com>
2024-12-25 14:01:18 +0100

    day 25 cleanup

--- 2024/Haskell/src/Day25.hs
+++ 2024/Haskell/src/Day25.hs
@@ -1,38 +1,29 @@
 module Day25 where
 
-import Lib (parseAsciiMap, count)
-import Linear (_x, _y)
+import Lib (parseAsciiMap, count, (.:))
+import Linear (V2(V2))
 import Advent.Coord (Coord, origin)
-import Control.Lens (view)
 import Control.Monad (guard)
-import Data.List.Extra (splitOn, minimumOn, maximumOn, partition)
+import Data.List.Extra (splitOn, partition)
 import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
 
-heights :: ((Coord -> Int) -> [Coord] -> Coord) -> Set (Coord) -> [Int]
-heights comp kl = flip map [0..4] $ \x ->
-  view _y
-  . comp (view _y)
-  . filter ((== x) . view _x)
-  $ Set.toList kl
+heights :: ([Int] -> Int) -> [Coord] -> [Int]
+heights comp kl = map f [0..4]
+  where f col = comp [ y | V2 x y <- kl, x == col ]
 
 solve :: [[Int]] -> [[Int]] -> Int
-solve locks keys = count id $ do
-  lock <- locks
-  key <- keys
-  pure $ and (zipWith (<) lock key)
+solve = count (and . uncurry (zipWith (<))) .: liftA2 (,)
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "../data/day25.in"
   let (locks, keys) = input
-      locks' = map (heights maximumOn) locks
-      keys'  = map (heights minimumOn) keys
+      locks' = map (heights maximum) locks
+      keys'  = map (heights minimum) keys
   print $ solve locks' keys'
    
-parseInput :: String -> ([Set Coord], [Set Coord])
-parseInput = partition (origin `Set.member`) . map (Map.keysSet . parseAsciiMap f) . splitOn "\n\n"
+parseInput :: String -> ([[Coord]], [[Coord]])
+parseInput = partition (origin `elem`) . map (Map.keys . parseAsciiMap f) . splitOn "\n\n"
   where f c = guard (c == '#') *> Just c
 
 -- 3155
