03ff229e6cf8438877e465e28bc24b739eea9208
Sondre Lunde <sondreslunde@gmail.com>
2025-12-07 20:19:17 +0100

    day 7 knot-tie for both parts

--- 2025/Haskell/src/Day07.hs
+++ 2025/Haskell/src/Day07.hs
@@ -2,19 +2,23 @@ module Day07 where
 
 import Lib (parseAsciiMap, count)
 import Advent.Coord (south, west, east, Coord)
-import Advent.Search (bfs)
 import Control.Lens ((<&>))
 import Data.Map (Map)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 pathMap :: Map Coord Char -> Map Coord [Coord]
 pathMap input = flip Map.mapWithKey input $ \k ->
   \case '^' -> [k + west, k + east]
         c   -> [k + south]
 
-part1 :: Map Coord [Coord] -> Coord -> [Coord]
-part1 input start = bfs [start] nexts
-  where nexts pos = Map.findWithDefault [] pos input
+part1 :: Map Coord [Coord] -> Map Coord (Set.Set Coord)
+part1 input = mp
+  where
+    mp = flip Map.mapWithKey input $ \k v ->
+            case v of
+              [x] -> Map.findWithDefault Set.empty x mp
+              xs  -> Set.insert k . Set.unions $ map (mp Map.!) v
 
 part2 :: Map Coord [Coord] -> Map Coord Int
 part2 input = mp
@@ -27,8 +31,8 @@ main = do
   input <- parseAsciiMap Just <$> readFile "../data/day07.in"
   let start = head . Map.keys $ Map.filter (== 'S') input
       mp = pathMap input
-  print $ count ((== Just '^') . (`Map.lookup` input)) $ part1 mp start
-  print $ (Map.! start) $ part2 mp
+  print . length . (Map.! start) $ part1 mp
+  print . (Map.! start) $ part2 mp
     
 -- 1541
 -- 80158285728929
