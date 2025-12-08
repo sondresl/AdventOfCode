252d35246180780e9865163b1d93647a2aaa6e02
Sondre Lunde <sondreslunde@gmail.com>
2025-12-08 15:09:06 +0100

    cleanup day 8

--- 2025/Haskell/src/Day08.hs
+++ 2025/Haskell/src/Day08.hs
@@ -4,11 +4,11 @@ import Lib (allNums, combinations, tuple)
 import Data.List.Extra (sortOn, sort, find)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Linear (V3(..), distance, _x)
+import Linear (V3(..), qd, _x)
 import Control.Lens (view)
 import Data.Ord (Down(Down))
 
-type JunctionBox = V3 Double
+type JunctionBox = V3 Int
 type JunctionPair = (JunctionBox, JunctionBox)
 type Circuits = Set (Set JunctionBox)
 
@@ -22,7 +22,7 @@ solve boxes = go (Set.fromList $ map Set.singleton boxes) js
   where
 
     js :: [Set JunctionBox]
-    js = map (\(x,y) -> Set.fromList [x,y]) . sortOn (uncurry distance) $ map tuple $ combinations 2 boxes
+    js = map (\(x,y) -> Set.fromList [x,y]) . sortOn (uncurry qd) . map tuple $ combinations 2 boxes
 
     go :: Circuits -> [Set JunctionBox] -> [(Set JunctionBox, Circuits)]
     go _ [] = []
@@ -34,10 +34,10 @@ main = do
   input <- parseInput <$> readFile "../data/day08.in"
   let states = solve input
   print $ product . take 3 . sortOn Down . map Set.size . Set.toList . snd . (!! 999) $ states
-  print $ product . Set.map (round . view _x) . fst <$> find ((== 1) . Set.size . snd) states
+  print $ product . Set.map (view _x) . fst <$> find ((== 1) . Set.size . snd) states
 
 parseInput :: String -> [JunctionBox]
-parseInput = map ((\[x,y,z] -> V3 x y z) . map fromIntegral . allNums) . lines
+parseInput = map ((\[x,y,z] -> V3 x y z) . allNums) . lines
 
 -- 175500
 -- Just 6934702555
