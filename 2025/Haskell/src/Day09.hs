9411d692148503680d138005262fbf9a72c22acf
Sondre Lunde <sondreslunde@gmail.com>
2025-12-09 21:35:10 +0100

    cleanup day 9

--- 2025/Haskell/src/Day09.hs
+++ 2025/Haskell/src/Day09.hs
@@ -1,41 +1,33 @@
 module Day09 where
 
-import Lib (allNums, combinations, tuple, zipWithTail')
+import Lib (allNums, combinations, tuple, zipWithTail', (.:))
 import Advent.Coord (Coord)
 import qualified Data.Map as Map
 import Linear (V2(..))
 
 area :: Coord -> Coord -> Int
-area (V2 x y) (V2 a b) = (abs (a - x) + 1) * (abs (b - y) + 1)
+area = product .: ((+1) .: liftA2 (abs .: (-)))
 
-part2 :: [Coord] -> Int
-part2 corners = maximum . map (uncurry area) $ filter (uncurry isValid) combos
+part2 :: [Coord] -> [(Coord, Coord)]
+part2 corners = filter (uncurry isValid) . map tuple $ combinations 2 corners
   where
-    lines = zipWithTail' corners
-    vertical = filter (\(V2 x _, V2 x' _) -> x == x') lines
-    horizontal = filter (\(V2 _ y, V2 _ y') -> y == y') lines
-    combos = map tuple $ combinations 2 corners
+    lines = map (\(from, to) -> (liftA2 min from to, liftA2 max from to)) $ zipWithTail' corners
 
-    okHori top@(V2 x y, V2 x' y') bot@(V2 f g, V2 f' g') (V2 a b, V2 a' b') =
-       (min b b' >= y || max b b' <= g) || (a <= x || a >= x')
+    isValid :: Coord -> Coord -> Bool
+    isValid from to = all (valid mi ma) lines
+      where
+        mi = liftA2 min from to
+        ma = liftA2 max from to 
 
-    okVert left@(V2 x y, V2 x' y') right@(V2 f g, V2 f' g') (V2 a b, V2 a' b') =
-      max a a' <= x || min a a' >= f || b <= y || b >= y'
-
-    isValid (V2 x y) (V2 x' y') | x == x' || y == y' = False
-    isValid (V2 x y) (V2 x' y') =
-      let top = (V2 (min x x') (max y y'), V2 (max x x') (max y y'))
-          bot = (V2 (min x x') (min y y'), V2 (max x x') (min y y'))
-          left = (V2 (min x x') (min y y'), V2 (min x x') (max y y'))
-          right = (V2 (max x x') (min y y'), V2 (max x x') (max y y'))
-       in    all (okHori top bot)  vertical
-          && all (okVert left right) horizontal
+    valid :: Coord -> Coord -> (Coord, Coord) -> Bool
+    valid from to (mi, ma) = or (liftA2 (<=) to mi) || or (liftA2 (>=) from ma)
 
 main :: IO ()
 main = do
   input <- map (uncurry V2 . tuple . allNums) . lines <$> readFile "../data/day09.in"
-  print . maximum . map (uncurry area . tuple) $ combinations 2 input
-  print $ part2 input
+  let solve = maximum . map (uncurry area)
+  print . solve . map tuple . combinations 2 $ input
+  print . solve . part2 $ input
 
 -- 4739623064
 -- 1654141440
